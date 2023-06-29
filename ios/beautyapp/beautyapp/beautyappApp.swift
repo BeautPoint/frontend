b2c70894f9a221bc797ddd53ee9166486dbda20a
JoEunsoo <dcpg95@gmail.com>
2023-06-29 09:33:00 +0900

    INIT : 프로젝트 초기 환경 구성

---
+++ ios/beautyapp/beautyapp/beautyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  beautyappApp.swift
+//  beautyapp
+//
+//  Created by EunSooJ on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct beautyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
