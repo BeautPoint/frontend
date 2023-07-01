b5158a78f0b42bdd8b1c4b274c14549845d564b4
JoEunsoo <dcpg95@gmail.com>
2023-07-01 09:39:17 +0900

    CHORE : babel 환경설정 수정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
