915959375d56670f42c5688e0ee9b0ea9bea7029
JoEunsoo <dcpg95@gmail.com>
2023-07-05 09:41:20 +0900

    FEAT : 사용자 인증 컴포넌트 구현

---
+++ src/components/auth/index.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+import GoogleOauthButton from './googleOauth.component';
+
+function SingIn() {
+  return (
+    <View>
+      <Text>hi</Text>
+      <GoogleOauthButton />
+    </View>
+  );
+}
+
+export default SingIn;
