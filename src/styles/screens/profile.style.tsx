347ceda11c93712dfc273c8dd50807d7cb5c21bf
JoEunsoo <dcpg95@gmail.com>
2023-07-18 17:17:11 +0900

    STYLE : 스크린 컴포넌트 styled 구현

---
+++ src/styles/screens/profile.style.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+const ProfileLayOut = styled.View`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+`;
+
+const TitleText = styled.Text`
+  font-size: 18px;
+`;
+
+export {ProfileLayOut, TitleText};
