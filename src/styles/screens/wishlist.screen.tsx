7e0419c30eec1ba1672bdcb039312874b627730b
JoEunsoo <dcpg95@gmail.com>
2023-07-18 17:17:18 +0900

    STYLE : 스크린 컴포넌트 styled 구현

---
+++ src/styles/screens/wishlist.screen.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+const WishListLayOut = styled.View`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+`;
+
+const TitleText = styled.Text`
+  font-size: 18px;
+`;
+
+export {WishListLayOut, TitleText};
