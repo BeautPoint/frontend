120432d963971745a9d8899a7cc6813cebdc6ef0
JoEunsoo <dcpg95@gmail.com>
2023-07-19 17:22:42 +0900

    STYLE : 사용자 인증 컴포넌트 styled 요소 추가

---
+++ src/styles/auth/signin.style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+const SignInLayOut = styled.View`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export {SignInLayOut};
