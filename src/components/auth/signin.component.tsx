27ba0bfe158e229f44b1419a23bca09b8340c3d5
JoEunsoo <dcpg95@gmail.com>
2023-07-28 15:08:02 +0900

    REFACTOR : import 경로 변경

--- src/components/auth/signin.component.tsx
+++ src/components/auth/signin.component.tsx
@@ -1,8 +1,8 @@
 import {Platform} from 'react-native';
-import AppleOauthButton from '@/components/auth/appleOauth.component';
-import GoogleOauthButton from '@/components/auth/googleOauth.component';
-import KakaoOauthButton from '@/components/auth/kakaoOauth.component';
-import NaverOauthButton from '@/components/auth/naverOauth.component';
+import AppleOauthButton from '@/components/auth/social/appleOauth.component';
+import GoogleOauthButton from '@/components/auth/social/googleOauth.component';
+import KakaoOauthButton from '@/components/auth/social/kakaoOauth.component';
+import NaverOauthButton from '@/components/auth/social/naverOauth.component';
 import * as S from '@/styles/auth/signin.style';
 
 function SignIn() {
