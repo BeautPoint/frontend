f9b8f042484e56a30c13fbd5eb8b82d32b85fef2
JoEunsoo <dcpg95@gmail.com>
2023-08-04 10:43:03 +0900

    DESIGN : 앱 전역 styled 구현

--- src/styles/global.style.tsx
+++ src/styles/global.style.tsx
@@ -1,4 +1,25 @@
-import styled from 'styled-components/native';
+import styled, {ThemeProvider} from 'styled-components/native';
+import {StyleSheet} from 'react-native';
+
+interface PropsType {
+  text: {size?: string; weight?: string; color?: string};
+}
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    padding: 24,
+    flex: 1,
+  },
+  titleText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 18,
+    color: '#333',
+  },
+  paragraph: {
+    marginVerical: 8,
+    lineHeight: 20,
+  },
+});
 
 const Container = styled.SafeAreaView`
   width: 100%;
@@ -6,4 +27,17 @@ const Container = styled.SafeAreaView`
   position: relative;
 `;
 
-export {Container};
+/** font
+ * neurimboGothicRegular
+ * Pretendard
+ *  **/
+
+const AppText = styled.Text<PropsType['text']>`
+  font-size: ${({size}) => size || '14px'};
+  /* font-weight: ${({weight}) => weight || 'normal'}; */
+  color: ${({color}) => color || 'black'};
+  font-family: ${({weight}) =>
+    weight ? 'Pretendard-' + weight : 'Pretendard-Regular'};
+`;
+
+export {Container, AppText};
