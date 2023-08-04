8d8e26b850a4a3950bb74a1f017bf9172e063867
JoEunsoo <dcpg95@gmail.com>
2023-08-04 10:44:53 +0900

    DESIGN : 로그인 화면 styled 구현

--- src/styles/auth/signin.style.tsx
+++ src/styles/auth/signin.style.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 const SignInLayOut = styled.View`
   width: 100%;
-  height: 200px;
+  height: 160px;
   display: flex;
   justify-content: space-around;
   align-items: center;
