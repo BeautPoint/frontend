fcca51187ef2709821d9ff2b980018e5c981bc2b
JoEunsoo <dcpg95@gmail.com>
2023-08-05 10:45:37 +0900

    DESIGN : 회원가입 사용자 입력폼 styled 구현

---
+++ src/styles/auth/userinfoform.style.tsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components/native';
+
+interface PropType {
+  genderOption: {isPressed?: boolean};
+  text: {size?: string; color?: string};
+}
+
+/** 공통 스타일 **/
+
+const ButtonCommonStyle = styled.Pressable`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0.5px solid #adadad;
+  margin: 10px 0;
+  width: 105px;
+  height: 40px;
+`;
+
+const ViewCommonStyle = styled.View`
+  width: 100%;
+  height: 100px;
+`;
+
+/** 개별 스타일 **/
+
+const UserInfoFormLayOut = styled.View`
+  width: 100%;
+`;
+
+const DescriptionBox = styled(ViewCommonStyle)``;
+const Text = styled.Text<PropType['text']>`
+  font-size: ${({size}) => size || '14px'};
+  color: ${({color}) => color || 'black'};
+`;
+const ProfileDetail = styled(ViewCommonStyle)``;
+
+const DateInputBox = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const DateInput = styled(ButtonCommonStyle)`
+  border-radius: 5px;
+  flex-direction: row;
+`;
+
+const InputIcon = styled.View`
+  position: absolute;
+  right: 15px;
+`;
+
+const GenderOptionBox = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Label = styled.View`
+  width: 100%;
+`;
+
+const GenderOption = styled(ButtonCommonStyle)<PropType['genderOption']>`
+  width: 48%;
+  height: 40px;
+  border-radius: 5px;
+  background: ${({isPressed}) => (isPressed ? '#b1b1b1' : 'transparent')};
+`;
+
+export {
+  UserInfoFormLayOut,
+  DescriptionBox,
+  Text,
+  ProfileDetail,
+  DateInputBox,
+  Label,
+  DateInput,
+  InputIcon,
+  GenderOptionBox,
+  GenderOption,
+};
