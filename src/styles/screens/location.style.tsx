feafa2d79472f0ad78dcd36efe0c83009974d1a8
JoEunsoo <dcpg95@gmail.com>
2023-08-05 10:48:10 +0900

    DESIGN : 내주변 화면 UI styled 구현

--- src/styles/screens/location.style.tsx
+++ src/styles/screens/location.style.tsx
@@ -1,3 +1,4 @@
+import {Shadow} from 'react-native-shadow-2';
 import styled from 'styled-components/native';
 
 const LocationLayOut = styled.View`
@@ -5,4 +6,58 @@ const LocationLayOut = styled.View`
   height: 100%;
 `;
 
-export {LocationLayOut};
+const SearchInputBox = styled.View`
+  width: 100%;
+  position: absolute;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  top: 10px;
+`;
+
+const MyLocation = styled.Pressable`
+  position: absolute;
+  z-index: 1;
+  right: 25px;
+  top: 70px;
+  width: 36px;
+  height: 36px;
+  background: #ffffff;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BoxShadow = styled(Shadow).attrs({
+  startColor: 'rgba(113, 121, 132, 0.20)',
+  offset: [2, 4],
+})`
+  border-radius: 10px;
+`;
+
+const IconBox = styled.View`
+  margin-right: 10px;
+`;
+
+const SearchButton = styled.Pressable`
+  width: 320px;
+  height: 48px;
+  z-index: 2;
+  height: 48px;
+  border-radius: 12px;
+  background: #f4f5f6;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  padding: 0 15px;
+`;
+
+export {
+  LocationLayOut,
+  SearchButton,
+  SearchInputBox,
+  BoxShadow,
+  IconBox,
+  MyLocation,
+};
