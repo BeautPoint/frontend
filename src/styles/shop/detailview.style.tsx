180f85662524b5e5df8ff071902545145fbf3473
JoEunsoo <dcpg95@gmail.com>
2023-08-05 10:50:44 +0900

    DESIGN : 병원 및 샵 상세보기 화면의 styled 구현

---
+++ src/styles/shop/detailview.style.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+
+interface PropsType {
+  tabs: {isSelected: boolean};
+}
+
+const CommonStyle = styled.View`
+  display: flex;
+  align-items: center;
+`;
+
+const DetailViewLayOut = styled.View`
+  width: 100%;
+  margin-top: 10px;
+`;
+const TabBox = styled(CommonStyle)`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 40px;
+  background: #ffffff;
+  border-bottom-width: 3px;
+  border-bottom-color: #f4f5f6;
+`;
+const Tabs = styled.Pressable<PropsType['tabs']>`
+  width: 33%;
+  height: 40px;
+  border-bottom-width: ${({isSelected}) => (isSelected ? '2px' : '0px')};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const DetailTabs = styled.View`
+  padding: 30px 20px;
+  background: #ffffff;
+  min-height: 150px;
+`;
+
+const Introduction = styled.View`
+  margin-bottom: 10px;
+`;
+
+const Description = styled.View``;
+
+export {DetailViewLayOut, TabBox, Tabs, DetailTabs, Introduction, Description};
