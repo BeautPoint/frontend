4f3fa92620da81a90a4303817ad26dbc55773b06
JoEunsoo <dcpg95@gmail.com>
2023-08-11 16:04:32 +0900

    FEAT : 앱의 하단 tab Navigation Bar 구현

---
+++ src/navigation/tab.navigation.tsx
@@ -0,0 +1,78 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Platform, View} from 'react-native';
+import {useRecoilValue} from 'recoil';
+import navigationState from '@/recoil/navigation/navigation.recoil';
+import {AppText} from '@/styles/global.style';
+
+const Tab = createBottomTabNavigator();
+
+function TabNavigation() {
+  const {tabNavigationState} = useRecoilValue(navigationState);
+  return (
+    <>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: '#619BFF', // 활성화된 탭의 라벨 색상
+          tabBarInactiveTintColor: '#8C939C',
+          tabBarStyle: {
+            backgroundColor: '#ffffff',
+            height: Platform.OS === 'ios' ? 70 : 80,
+            paddingLeft: 10,
+            paddingRight: 10,
+          },
+          headerShown: false,
+        }}>
+        {tabNavigationState.map(li => {
+          return (
+            <Tab.Screen
+              key={li.id}
+              name={li.name}
+              component={li.component}
+              options={{
+                tabBarIcon: ({focused}) => {
+                  return (
+                    <View
+                      style={{
+                        marginTop: 20,
+                        height: 30,
+                      }}>
+                      <li.icon
+                        color={focused ? '#619BFF' : '#c9c9c9'}
+                        width={20}
+                        height={20}
+                      />
+                    </View>
+                  );
+                },
+                tabBarLabel: ({focused, color}) => (
+                  <View
+                    style={{
+                      flex: 1,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      marginBottom: Platform.OS === 'android' ? 15 : 5,
+                    }}>
+                    <AppText
+                      style={{
+                        height: 20,
+                        marginTop: Platform.OS === 'android' && focused ? 2 : 0,
+                        color,
+                        fontFamily: focused
+                          ? 'Pretendard-ExtraBold'
+                          : 'Pretendard',
+                        fontSize: 12,
+                      }}>
+                      {li.name}
+                    </AppText>
+                  </View>
+                ),
+              }}
+            />
+          );
+        })}
+      </Tab.Navigator>
+    </>
+  );
+}
+
+export default TabNavigation;
