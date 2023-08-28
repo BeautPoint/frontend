7fc2ff6ce3a47682db85f8b6d63607e0ff540a31
JoEunsoo <dcpg95@gmail.com>
2023-08-28 16:07:42 +0900

    FEAT : 커뮤니티 화면의 컴포넌트 구성

--- src/screens/main/community.screen.tsx
+++ src/screens/main/community.screen.tsx
@@ -1,8 +1,20 @@
+import CommunityHeader from '@/components/community/header.component';
+import CommunityPost from '@/components/community/post.component';
 import * as S from '@/styles/screens/community.style';
+import WriteIcon from '@/assets/icons/writeIcon.svg';
+
 function CommunityScreen() {
   return (
     <S.CommunityLayOut>
-      {/* <S.TitleText>Community</S.TitleText> */}
+      <CommunityHeader />
+      <S.ScrollViewBox>
+        <CommunityPost />
+        <CommunityPost />
+        <CommunityPost />
+      </S.ScrollViewBox>
+      <S.NewPostButton>
+        <WriteIcon color="#ffffff" />
+      </S.NewPostButton>
     </S.CommunityLayOut>
   );
 }
