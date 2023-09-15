ea4b94490ac7731430e9d22965c53eae5930d7c8
JoEunsoo <dcpg95@gmail.com>
2023-09-15 17:27:39 +0900

    DESIGN : 커뮤니티 게시물 상세보기 styled 구현

---
+++ src/styles/community/postdetail.style.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components/native';
+
+const DetailLayout = styled.View``;
+
+const PostMainSection = styled.View`
+  padding: 20px;
+  background: #ffffff;
+  margin-bottom: 2px;
+`;
+
+const MainBox = styled.View`
+  padding: 10px 0;
+`;
+const PostTitle = styled.View`
+  margin-bottom: 15px;
+`;
+const PostBody = styled.View``;
+
+const CommentSection = styled.View`
+  padding: 20px;
+  background: #ffffff;
+`;
+
+const CommentHeader = styled.View`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Label = styled.View`
+  margin-right: 10px;
+`;
+
+const Count = styled.View``;
+
+export {
+  DetailLayout,
+  PostMainSection,
+  MainBox,
+  PostTitle,
+  PostBody,
+  CommentSection,
+  CommentHeader,
+  Label,
+  Count,
+};
