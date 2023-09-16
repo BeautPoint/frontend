677d30cd7efbb37a1cd9277d0020eca5e782a941
JoEunsoo <dcpg95@gmail.com>
2023-09-16 17:09:52 +0900

    FEAT : 홈 화면 컴포넌트 비즈니스 로직 분리

---
+++ src/hooks/home/home.hook.ts
@@ -0,0 +1,28 @@
+import HomeState from '@/recoil/home/home.recoil';
+import {useRecoilState} from 'recoil';
+
+export const useHomeScreenHooks = () => {
+  const [state, setState] = useRecoilState(HomeState);
+
+  const shopLikeHandle = (shop: any) => {
+    console.log(shop);
+    setState(prev => {
+      const isTagIncluded = prev.likeShops.some(item => item.id === shop.id);
+      console.log(isTagIncluded);
+      const updatedSelectedTags = isTagIncluded
+        ? prev.likeShops.filter(item => item.id !== shop.id)
+        : [...prev.likeShops, shop];
+
+      /** tag 상태 값이 없는경우 버튼 비활성화 */
+      return {
+        ...prev,
+        likeShops: updatedSelectedTags,
+      };
+    });
+    // setState(prev => ({
+    //   ...prev,
+    //   likeButton: state.likeShops.id === id ? '' : id,
+    // }));
+  };
+  return {shopLikeHandle};
+};
