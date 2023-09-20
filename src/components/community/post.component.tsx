c6dd77de4ae6220b0d767d224bd41f3bc6a5064d
JoEunsoo <dcpg95@gmail.com>
2023-09-20 17:15:50 +0900

    REFACTOR : 커뮤니티 게시물 컴포넌트 api 연동 및 조건부 렌더링 추가

--- src/components/community/post.component.tsx
+++ src/components/community/post.component.tsx
@@ -1,63 +1,77 @@
 import * as S from '@/styles/community/post.style';
 import {AppText} from '@/styles/global.style';
 import DotsIcon from '@/assets/icons/dotsIcon.svg';
+import CommunityUserInfo from '@/components/community/userinfo.component';
+import {useCommunityQuery} from '@/api/community/post.api';
+import ThumbsupIcon from '@/assets/icons/thumbsupIcon.svg';
+import CommentIcon from '@/assets/icons/commentIcon.svg';
+import {useCommunityPosts} from '@/hooks/community/communityPosts.hook';
+import {useRecoilValue} from 'recoil';
+import communityState from '@/recoil/community/community.recoil';
+import {NavigationProps} from '@/types/stackprops';
+import {FlatList} from 'react-native';
 
-function CommunityPost() {
+function CommunityPost({navigation}: NavigationProps['community']) {
+  const {postData} = useCommunityQuery();
+  const {likeButtonHandle} = useCommunityPosts();
+  const {likeButton} = useRecoilValue(communityState);
+  const renderItem = ({item: post}) => {
+    const buttonisActived = likeButton === post.id ? '#619BFF' : '#9a9a9a';
+    return (
+      <S.PostContainer
+        key={post.id}
+        onPress={() => navigation.navigate('Community')}>
+        <CommunityUserInfo />
+        <S.MainBox>
+          <S.PostTitle>
+            <AppText size="16px" weight="SemiBold">
+              {post?.title}
+            </AppText>
+          </S.PostTitle>
+          <S.PostBody>
+            <AppText color="#484B56A6" numberOfLines={2}>
+              {post?.description}
+            </AppText>
+          </S.PostBody>
+          <S.TagBox>
+            <S.PostTag>
+              <AppText color="#BABFC4" size="12px">
+                궁금증
+              </AppText>
+            </S.PostTag>
+            <S.PostTag>
+              <AppText color="#BABFC4" size="12px">
+                SMP 두피
+              </AppText>
+            </S.PostTag>
+          </S.TagBox>
+        </S.MainBox>
+        <S.BottomBox>
+          <S.ActionButton onPress={() => likeButtonHandle(post?.id)}>
+            <S.ButtonIcon>
+              <ThumbsupIcon
+                width="30px"
+                height="20px"
+                color={buttonisActived}
+              />
+            </S.ButtonIcon>
+            <AppText color={buttonisActived}>좋아요</AppText>
+          </S.ActionButton>
+          <S.DividerLine />
+          <S.ActionButton onPress={() => navigation.navigate('Community')}>
+            <S.ButtonIcon>
+              <CommentIcon width="40px" height="20px" />
+            </S.ButtonIcon>
+            <AppText color="#9a9a9a">댓글</AppText>
+          </S.ActionButton>
+        </S.BottomBox>
+      </S.PostContainer>
+    );
+  };
+
   return (
     <S.PostLayout>
-      <S.TopBox>
-        <S.ProfileImage />
-        <S.UserInfo>
-          <S.UserName>
-            <AppText>닮은살걀</AppText>
-          </S.UserName>
-          <S.PostStats>
-            <S.CreatedAt>
-              <AppText size="12px">3분 전</AppText>
-            </S.CreatedAt>
-            <S.Views>
-              <AppText size="12px">조회수 123</AppText>
-            </S.Views>
-          </S.PostStats>
-        </S.UserInfo>
-        <S.PostSetButton>
-          <DotsIcon color="#B6B9BF" />
-        </S.PostSetButton>
-      </S.TopBox>
-      <S.MainBox>
-        <S.PostTitle>
-          <AppText size="16px" weight="SemiBold">
-            두피문신 질문
-          </AppText>
-        </S.PostTitle>
-        <S.PostBody>
-          <AppText color="#484B56A6">
-            두피 딱지 생기거나 부작용으로 고생하신 분 있으신가요? 딱지때매ㅜ 넘
-            고생이네요,,
-          </AppText>
-        </S.PostBody>
-        <S.TagBox>
-          <S.PostTag>
-            <AppText color="#BABFC4" size="12px">
-              궁금증
-            </AppText>
-          </S.PostTag>
-          <S.PostTag>
-            <AppText color="#BABFC4" size="12px">
-              SMP 두피
-            </AppText>
-          </S.PostTag>
-        </S.TagBox>
-      </S.MainBox>
-      <S.BottomBox>
-        <S.ActionButton>
-          <AppText>좋아요</AppText>
-        </S.ActionButton>
-        <S.DividerLine />
-        <S.ActionButton>
-          <AppText>댓글</AppText>
-        </S.ActionButton>
-      </S.BottomBox>
+      <FlatList data={postData} renderItem={renderItem} />
     </S.PostLayout>
   );
 }
