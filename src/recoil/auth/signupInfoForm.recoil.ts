39d8a8db306de25c87e78a794cd61564a82b8395
JoEunsoo <dcpg95@gmail.com>
2023-09-27 15:25:10 +0900

    ADD : recoil atom key 난수값 추가

--- src/recoil/auth/signupInfoForm.recoil.ts
+++ src/recoil/auth/signupInfoForm.recoil.ts
@@ -1,13 +1,38 @@
+import {randomString} from '@/utils/randomString.util';
 import {atom} from 'recoil';
 
 const signupState = atom({
-  key: 'signupState',
+  key: `signupState/${randomString()}`,
   default: {
-    userInfoForm: [
+    progressStep: 1,
+    birthInfo: [
       {id: 0, name: '연도', dateValue: ''},
       {id: 1, name: '월', dateValue: ''},
       {id: 2, name: '일', dateValue: ''},
     ],
+    genderInfo: [
+      {id: 0, name: '남자'},
+      {id: 1, name: '여자'},
+    ],
+    serviceCategory: [
+      {id: 0, description: 'SMP두피'},
+      {id: 1, description: '헤어라인'},
+      {id: 2, description: '눈썹문신'},
+      {id: 3, description: '아이라인'},
+      {id: 4, description: '입술문신'},
+      {id: 5, description: '흉터커버'},
+      {id: 6, description: '문신제거'},
+    ],
+    requirementInfo: [
+      {id: 0, description: '내위치에서 가까운 곳의 정보가 필요해요'},
+      {id: 1, description: '유명하고 실력있는 곳에 가고싶어요'},
+      {id: 2, description: '객관적이고 사실적인 리뷰를 찾고싶어요'},
+      {id: 3, description: '같은 고민을 공유하는 사람들과 정보 교류를 원해요'},
+    ],
+    selectedTags: [] as number[],
+    selectedServices: [] as {id: number; description: string}[],
+    selectedRequirement: [] as {id: number; description: string}[],
+    nextButtonActive: false,
   },
 });
 
