9fae4295e043a9a0a2f31ba940ea55111f274c2d
JoEunsoo <dcpg95@gmail.com>
2023-10-10 17:31:51 +0900

    REFACTOR : 검색 화면 공통 컴포넌트로 분리 및 지도 검색화면 컴포넌트명으로 변경

--- src/screens/search/searchview.screen.tsx
+++ src/screens/search/searchview.screen.tsx
@@ -1,56 +1,13 @@
-import {AppText} from '@/styles/global.style';
-import * as S from '@/styles/screens/searchview.style';
-import CancelIcon from '@/assets/icons/cancelIcon.svg';
-import SearchIcon from '@/assets/icons/searchIcon.svg';
-import BackIcon from '@/assets/icons/backIcon.svg';
+import {Fragment} from 'react';
+import SearchView from '@/components/common/searchView.component';
 import {NavigationProps} from '@/types/stackprops';
 
-function SearchView({navigation}: NavigationProps['location']) {
+function locationSearchScreen({navigation}: NavigationProps['location']) {
   return (
-    <S.SerachViewLayout>
-      <S.SearchInputBox>
-        <S.BackButton onPress={() => navigation.goBack()}>
-          <BackIcon />
-        </S.BackButton>
-        <S.SearchInput placeholder="시술 부위나 병원 명을 검색하세요" />
-        <S.SearchIconBox>
-          <SearchIcon color={'#337EFF'} />
-        </S.SearchIconBox>
-      </S.SearchInputBox>
-      <S.SearchHistoryBox>
-        <S.HistoryHeader>
-          <AppText size="16px" weight="bold">
-            최근찾아본
-          </AppText>
-          <S.DeleteAllKeyword>
-            <AppText size="12px" color="#717984">
-              전체 삭제
-            </AppText>
-          </S.DeleteAllKeyword>
-        </S.HistoryHeader>
-        <S.HistoryMain>
-          <S.KeywordTag>
-            <AppText size="14px">눈썹 정리</AppText>
-            <S.DeleteKeyword onPress={() => console.log('wdwed')}>
-              <CancelIcon />
-            </S.DeleteKeyword>
-          </S.KeywordTag>
-          <S.KeywordTag>
-            <AppText size="14px">탈모</AppText>
-            <S.DeleteKeyword>
-              <CancelIcon />
-            </S.DeleteKeyword>
-          </S.KeywordTag>
-          <S.KeywordTag>
-            <AppText size="14px">탈모</AppText>
-            <S.DeleteKeyword>
-              <CancelIcon />
-            </S.DeleteKeyword>
-          </S.KeywordTag>
-        </S.HistoryMain>
-      </S.SearchHistoryBox>
-    </S.SerachViewLayout>
+    <Fragment>
+      <SearchView navigation={navigation} />
+    </Fragment>
   );
 }
 
-export default SearchView;
+export default locationSearchScreen;
