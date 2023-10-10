e1e15295d39b50282c1e5e85ee7810327e2b20bd
JoEunsoo <dcpg95@gmail.com>
2023-10-10 17:32:45 +0900

    REFACTOR : 커뮤니티 화면의 게시물 컴포넌트에 navigation props 부여

--- src/screens/main/community.screen.tsx
+++ src/screens/main/community.screen.tsx
@@ -2,15 +2,15 @@ import CommunityHeader from '@/components/community/header.component';
 import CommunityPost from '@/components/community/post.component';
 import * as S from '@/styles/screens/community.style';
 import WriteIcon from '@/assets/icons/writeIcon.svg';
+import CommunityPostDetail from '@/components/community/detail/postdetail.component';
+import {NavigationProps} from '@/types/stackprops';
 
-function CommunityScreen() {
+function CommunityScreen({navigation}: NavigationProps['community']) {
   return (
     <S.CommunityLayOut>
       <CommunityHeader />
       <S.ScrollViewBox>
-        <CommunityPost />
-        <CommunityPost />
-        <CommunityPost />
+        <CommunityPost navigation={navigation} />
       </S.ScrollViewBox>
       <S.NewPostButton>
         <WriteIcon color="#ffffff" />
