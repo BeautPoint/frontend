06b8911d38333b7e156bd1eacef6046ec34f5b30
JoEunsoo <dcpg95@gmail.com>
2023-10-12 14:39:30 +0900

    FEAT : 공통 컴포넌트 드롭다운 UI 구현

---
+++ src/components/common/dropdown.component.tsx
@@ -0,0 +1,12 @@
+import * as S from '@/styles/common/dropdown.style';
+import {AppText} from '@/styles/global.style';
+
+function DropDown() {
+  return (
+    <S.DropDownLayOut>
+      <AppText>hi</AppText>
+    </S.DropDownLayOut>
+  );
+}
+
+export default DropDown;
