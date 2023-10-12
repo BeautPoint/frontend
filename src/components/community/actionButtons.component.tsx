9e7a60986f200989906393add3691001debbfe2d
JoEunsoo <dcpg95@gmail.com>
2023-10-12 14:40:42 +0900

    FEAT : 커뮤니티 화면의 상단 고정 버튼 컴포넌트 구현

---
+++ src/components/community/actionButtons.component.tsx
@@ -0,0 +1,18 @@
+import BellIcon from '@/assets/icons/bellIcon.svg';
+import SearchIcon from '@/assets/icons/searchIcon.svg';
+import * as S from '@/styles/community/header.style';
+
+function HeaderActionButtons() {
+  return (
+    <S.ActionButtonLayout>
+      <S.SearchButton>
+        <SearchIcon color="black" />
+      </S.SearchButton>
+      <S.NotificationButton>
+        <BellIcon color="black" width="17px" />
+      </S.NotificationButton>
+    </S.ActionButtonLayout>
+  );
+}
+
+export default HeaderActionButtons;
