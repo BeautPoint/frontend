c125f94af07389bb9a17ff55b93ee800f80df361
JoEunsoo <dcpg95@gmail.com>
2023-10-16 07:49:09 +0900

    FEAT : 커뮤니티 게시물 생성 화면 구현

---
+++ src/screens/community/createPost.scree.tsx
@@ -0,0 +1,8 @@
+import * as S from '@/styles/community/create/createPost.style';
+import {NavigationProps} from '@/types/stackprops';
+
+function CreatePostScreen({navigation}: NavigationProps['community']) {
+  return <S.CreatePostLayout />;
+}
+
+export default CreatePostScreen;
