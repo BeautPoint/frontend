a74553b152a5cbceab67acc2ccbef2ca0e7d9565
JoEunsoo <dcpg95@gmail.com>
2023-10-16 07:48:56 +0900

    FEAT : 버튼 클릭시 커뮤니티 게시물 생성 화면 전환 로직 추가

--- src/screens/main/community.screen.tsx
+++ src/screens/main/community.screen.tsx
@@ -2,23 +2,23 @@ import CommunityHeader from '@/components/community/header.component';
 import CommunityPost from '@/components/community/post.component';
 import * as S from '@/styles/screens/community.style';
 import WriteIcon from '@/assets/icons/writeIcon.svg';
-import CommunityPostDetail from '@/components/community/detail/postdetail.component';
 import {NavigationProps} from '@/types/stackprops';
-import DropDown from '@/components/common/dropdown.component';
 import {useRecoilValue} from 'recoil';
 import communityState from '@/recoil/community/community.recoil';
 import {useCommunityPosts} from '@/hooks/community/communityPosts.hook';
+import CreatePostScreen from '../community/createPost.scree';
 
 function CommunityScreen({navigation}: NavigationProps['community']) {
   const {showReportDropdown} = useRecoilValue(communityState);
   const {dropdownBackgroundHandle} = useCommunityPosts();
+  const showShopDetail = false;
   return (
     <S.CommunityLayOut>
       <CommunityHeader />
       <S.ScrollViewBox>
         <CommunityPost navigation={navigation} />
       </S.ScrollViewBox>
-      <S.NewPostButton>
+      <S.NewPostButton onPress={() => navigation.navigate('CreatePost')}>
         <WriteIcon color="#ffffff" />
       </S.NewPostButton>
       {showReportDropdown ? (
@@ -26,6 +26,7 @@ function CommunityScreen({navigation}: NavigationProps['community']) {
           onPress={() => dropdownBackgroundHandle()}
         />
       ) : null}
+      {showShopDetail && <CreatePostScreen navigation={navigation} />}
     </S.CommunityLayOut>
   );
 }
