b4ba159f7e5d71c5c574929f53a806b418157a6f
JoEunsoo <dcpg95@gmail.com>
2023-10-18 07:50:38 +0900

    DESIGN : 드랍다운 컴포넌트 styled 조건부 스타일 추가

--- src/styles/common/dropdown.style.tsx
+++ src/styles/common/dropdown.style.tsx
@@ -1,19 +1,43 @@
+import {Shadow} from 'react-native-shadow-2';
 import styled from 'styled-components/native';
+import DropShadow from 'react-native-drop-shadow';
 
 interface PropType {
-  dropDown: {width?: string; height?: string};
+  dropDown: {width?: string; height?: string | number};
+  selectBox: {width?: string; height?: string | number};
 }
 
 const DropDownLayOut = styled.View<PropType['dropDown']>`
-  width: ${({width}) => width || '80px'};
-  height: ${({height}) => height || '30px'};
-  border: 1px solid;
-  border-radius: 5px;
+  width: 100%;
+  height: 100%;
+  /* border: 1px solid; */
+  display: flex;
+  align-items: center;
 `;
 
 const Text = styled.Text``;
 
-const SelectBox = styled.View``;
+const SelectBox = styled.View<PropType['selectBox']>`
+  width: ${({width}) => width || '80px'};
+  height: ${({height}) => height || '100px'};
+  background: #ffffff;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: absolute;
+  z-index: 1;
+`;
 const SelectList = styled.View``;
+const BoxShadow = styled(Shadow).attrs({
+  startColor: '#aaa8a8',
+  distance: 3,
+  offset: [0, 2],
+})<PropType['selectBox']>`
+  width: 76px;
+  height: ${({height}) => height || '100px'};
+  border-radius: 5px;
+`;
+// const BoxShadow = styled.View``;
 
-export {DropDownLayOut, SelectBox, SelectList, Text};
+export {DropDownLayOut, SelectBox, SelectList, Text, BoxShadow};
