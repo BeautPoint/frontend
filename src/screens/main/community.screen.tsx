83b3587fcc3798f7523ba37ca886338557851a71
JoEunsoo <dcpg95@gmail.com>
2023-10-27 08:53:27 +0900

    REACTOR : 커뮤니티 화면 컴포넌트의 신고 기능 활성화 로직 추가

--- src/screens/main/community.screen.tsx
+++ src/screens/main/community.screen.tsx
@@ -4,8 +4,14 @@ import * as S from '@/styles/screens/community.style';
 import WriteIcon from '@/assets/icons/writeIcon.svg';
 import CommunityPostDetail from '@/components/community/detail/postdetail.component';
 import {NavigationProps} from '@/types/stackprops';
+import DropDown from '@/components/common/dropdown.component';
+import {useRecoilValue} from 'recoil';
+import communityState from '@/recoil/community/community.recoil';
+import {useCommunityPosts} from '@/hooks/community/communityPosts.hook';
 
 function CommunityScreen({navigation}: NavigationProps['community']) {
+  const {showReportDropdown} = useRecoilValue(communityState);
+  const {dropdownBackgroundHandle} = useCommunityPosts();
   return (
     <S.CommunityLayOut>
       <CommunityHeader />
@@ -15,6 +21,11 @@ function CommunityScreen({navigation}: NavigationProps['community']) {
       <S.NewPostButton>
         <WriteIcon color="#ffffff" />
       </S.NewPostButton>
+      {showReportDropdown ? (
+        <S.ReportDropdownBackground
+          onPress={() => dropdownBackgroundHandle()}
+        />
+      ) : null}
     </S.CommunityLayOut>
   );
 }
