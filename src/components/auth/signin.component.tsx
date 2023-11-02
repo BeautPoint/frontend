020c933d7f712e8d38104d79f6daa6d9565abf83
JoEunsoo <dcpg95@gmail.com>
2023-11-02 17:53:24 +0900

    FEAT : 로그인 컴포넌트 화면의 로그아웃 로직 구현

--- src/components/auth/signin.component.tsx
+++ src/components/auth/signin.component.tsx
@@ -1,15 +1,21 @@
-import {Platform} from 'react-native';
+import {Platform, Pressable, View} from 'react-native';
 import AppleOauthButton from '@/components/auth/social/appleOauth.component';
 import GoogleOauthButton from '@/components/auth/social/googleOauth.component';
 import KakaoOauthButton from '@/components/auth/social/kakaoOauth.component';
 import NaverOauthButton from '@/components/auth/social/naverOauth.component';
 import * as S from '@/styles/auth/signin.style';
+import {AppText} from '@/styles/global.style';
+import {useNaverOauth} from '@/hooks/auth/oauth.hooks';
 
 function SignIn() {
+  const {logout} = useNaverOauth();
   return (
     <S.SignInLayOut>
       <KakaoOauthButton />
       <NaverOauthButton />
+      <Pressable onPress={() => logout()}>
+        <AppText>로그아웃</AppText>
+      </Pressable>
       {Platform.OS === 'android' ? <GoogleOauthButton /> : <AppleOauthButton />}
     </S.SignInLayOut>
   );
