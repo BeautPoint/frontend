270cd6e843e6f7f7959ca713f3a46b85c908e63e
JoEunsoo <dcpg95@gmail.com>
2023-11-08 18:09:55 +0900

    REFACTOR : 로직 수정 및 샵 미리보기 컴포넌트 연동

--- src/components/common/bottomsheet.component.tsx
+++ src/components/common/bottomsheet.component.tsx
@@ -2,48 +2,112 @@ import * as S from '@/styles/common/bottomsheet.style';
 import {NavigationProps} from '@/types/stackprops';
 import {calculateBottom} from '@/utils/calculateheight.util';
 import * as React from 'react';
-import {Dimensions} from 'react-native';
+import {Dimensions, Platform} from 'react-native';
 import {Animated, PanResponder} from 'react-native';
 import ShopPreview from '../shop/preview.component';
+import ShopDetailScreen from '@/screens/shop/detail.screen';
+import ShopOverView from '../shop/overview.component';
+import ShopDetailView from '../shop/detailview.component';
+import {useRecoilValue, useSetRecoilState} from 'recoil';
+import shopState from '@/recoil/shop/shop.recoil';
+import {useBottomSheetHook} from '@/hooks/shop/bottomsheet.hook';
 
 const {height} = Dimensions.get('screen');
 
 function BottomSheet({navigation}: NavigationProps['location']) {
   // const {panResponder, panY} = useDragAnimation();
+  const dragStandardHeight = height * 0.26;
+  const maxHeight = height * 0.7;
+  const minHeight = height * 0.1;
+  const maximumTranslateY = minHeight - maxHeight;
+  const minimumTranslateY = 0;
+
+  const {previewFullScreen, isButtonPressed} = useRecoilValue(shopState);
+  const {dragHandle} = useBottomSheetHook();
+  const setShopInfo = useSetRecoilState(shopState);
+
   const translationY = React.useRef(0);
   const panY = React.useRef(new Animated.Value(0)).current;
-  const panResponder = PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: (evt, gesture) => {
-      panY.setValue(gesture.dy);
-      translationY.current = gesture.dy;
-    },
-    onPanResponderRelease: () => {
-      // position.extractOffset();
-      Animated.spring(panY, {
-        toValue: 0,
-        useNativeDriver: false,
-      }).start();
-
-      if (translationY.current < -heights) {
-        navigation.navigate('ShopDetails');
-      }
-    },
+  const panResponder = dragHandle({
+    height,
+    translationY,
+    panY,
   });
-  const heights = height * 0.26;
+
+  // const panResponder = dragHandle({
+  //   height,
+  //   translationY,
+  //   panY,
+  // });
+
+  // const previewHandle = (pram: boolean) => {
+  //   setShopInfo(prev => ({
+  //     ...prev,
+  //     previewFullScreen: pram,
+  //   }));
+  // };
+
+  // const panResponder = PanResponder.create({
+  //   onStartShouldSetPanResponder: () => true,
+
+  //   onPanResponderGrant: () => {
+  //     panY.setOffset(translationY.current);
+  //   },
+  //   onPanResponderMove: (evt, gesture) => {
+  //     panY.setValue(gesture.dy);
+  //     console.log(-dragStandardHeight);
+  //   },
+  //   onPanResponderRelease: (evt, gesture) => {
+  //     panY.flattenOffset();
+  //     console.log(gesture.dy > -dragStandardHeight);
+  //     console.log('ㅎㄷㄴㅅ : ' + -dragStandardHeight);
+
+  //     if (gesture.dy > dragStandardHeight) {
+  //       springAnimation('down');
+  //       previewHandle(false);
+  //     } else {
+  //       springAnimation('up');
+  //       previewHandle(true);
+  //     }
+  //   },
+  // });
+
+  // const springAnimation = (direction: 'up' | 'down') => {
+  //   console.log('direction', direction);
+  //   translationY.current =
+  //     direction === 'down' ? minimumTranslateY : maximumTranslateY;
+  //   Animated.spring(panY, {
+  //     toValue: translationY.current,
+  //     useNativeDriver: true,
+  //   }).start();
+  // };
+
+  console.log('풀 스크린 :' + previewFullScreen);
 
   return (
     <S.AnimatedView
       style={{
-        transform: [{translateY: panY}],
+        transform: [
+          {
+            translateY: panY.interpolate({
+              inputRange: [maximumTranslateY, minimumTranslateY],
+              outputRange: [maximumTranslateY, minimumTranslateY],
+              extrapolate: 'clamp',
+            }),
+          },
+        ],
       }}
-      bottom={calculateBottom(height)}
+      bottom={Platform.OS === 'ios'}
       {...panResponder.panHandlers}>
       <S.ShopInfoLayOut>
         <S.HandleSection>
-          <S.DragHandle />
+          {!previewFullScreen ? <S.DragHandle /> : null}
         </S.HandleSection>
-        <ShopPreview />
+        {previewFullScreen ? (
+          <ShopDetailScreen navigation={navigation} />
+        ) : (
+          <ShopPreview />
+        )}
       </S.ShopInfoLayOut>
     </S.AnimatedView>
   );
