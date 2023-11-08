1376f2d28981dcf5bcb08ac77556587306f36e85
JoEunsoo <dcpg95@gmail.com>
2023-11-08 18:10:49 +0900

    FEAT : 드롭다운 컴포넌트 구현

--- src/components/common/dropdown.component.tsx
+++ src/components/common/dropdown.component.tsx
@@ -1,10 +1,23 @@
 import * as S from '@/styles/common/dropdown.style';
 import {AppText} from '@/styles/global.style';
+import {Dimensions, Platform, StyleSheet} from 'react-native';
+import DropShadow from 'react-native-drop-shadow';
+import {Shadow} from 'react-native-shadow-2';
 
-function DropDown() {
+const {width, height} = Dimensions.get('screen');
+
+function DropDown({dropDownHeight}: {dropDownHeight: string}) {
   return (
-    <S.DropDownLayOut>
-      <AppText>hi</AppText>
+    <S.DropDownLayOut width={width} height={height}>
+      <S.SelectBox height={dropDownHeight}>
+        <AppText size="13px" color="#8b8b8b">
+          게시글 신고
+        </AppText>
+        <AppText size="13px" color="#8b8b8b">
+          사용자 신고
+        </AppText>
+      </S.SelectBox>
+      <S.BoxShadow height={dropDownHeight} />
     </S.DropDownLayOut>
   );
 }
