c875368f7578c30d09415414e8e8e88dfdfbf16e
JoEunsoo <dcpg95@gmail.com>
2023-11-09 15:31:01 +0900

    FEAT : 사업장 이미지 컴포넌트 구현

---
+++ src/components/common/shopImage.component.tsx
@@ -0,0 +1,36 @@
+import * as S from '@/styles/common/shopImage.style';
+import HeartIcon from '@/assets/icons/heartIcon.svg';
+import {useRecoilValue} from 'recoil';
+import HomeState from '@/recoil/home/home.recoil';
+import {useHomeScreenHooks} from '@/hooks/home/home.hook';
+
+function ShopImage({shopData}) {
+  const {shopList, likeShops} = useRecoilValue(HomeState);
+  const {shopLikeHandle} = useHomeScreenHooks();
+
+  return (
+    <S.ShopImageLayout>
+      <S.LikeButton onPress={() => shopLikeHandle(shopData)}>
+        <HeartIcon
+          color="#ffffff"
+          fill={
+            likeShops.some(list => list.id === shopData.id) ? '#ffffff' : 'none'
+          }
+          strokeWidth="2"
+        />
+      </S.LikeButton>
+      <S.GradientImage
+        start={{x: 0, y: 0}}
+        end={{x: 0, y: 1}}
+        colors={[
+          'rgba(0, 0, 0, 0.2)',
+          'rgba(0, 0, 0, 0.1)',
+          'rgba(0, 0, 0, 0.1)',
+        ]}
+      />
+      <S.Image source={{uri: shopData.uri}} />
+    </S.ShopImageLayout>
+  );
+}
+
+export default ShopImage;
