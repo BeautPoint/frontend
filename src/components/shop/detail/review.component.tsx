cbed022e23682f46bb8edfcc7ffbb6654b8e30af
JoEunsoo <dcpg95@gmail.com>
2023-11-16 07:59:51 +0900

    ADD : 사업장 리뷰 컴포넌트의 버튼 클릭시 리뷰 작성 네비게이션 로직 추가

--- src/components/shop/detail/review.component.tsx
+++ src/components/shop/detail/review.component.tsx
@@ -2,9 +2,14 @@ import shopState from '@/recoil/shop/shop.recoil';
 import {AppText} from '@/styles/global.style';
 import * as S from '@/styles/shop/detail/review.style';
 import {useRecoilValue} from 'recoil';
+import CommentIcon from '@/assets/icons/shopDetail/commentIcon.svg';
+import {NavigationProps} from '@/types/stackprops';
+import BriefReviewScreen from '../review/briefReview.component';
 
-function ShopReview() {
+function ShopReview({navigation}: NavigationProps['shopDetails']) {
   const {BriefReviewList} = useRecoilValue(shopState);
+  const showScreen = false;
+  console.log(navigation);
   return (
     <S.ReviewLayout>
       <S.BriefReview>
@@ -12,7 +17,10 @@ function ShopReview() {
           <AppText size="15px" weight="SemiBold">
             이런 점이 좋았어요
           </AppText>
-          <S.ActionButton>
+          <S.ActionButton onPress={() => navigation.navigate('BriefReiview')}>
+            <S.IconBox>
+              <CommentIcon />
+            </S.IconBox>
             <AppText>나도 참여</AppText>
           </S.ActionButton>
         </S.TitleBox>
@@ -33,11 +41,21 @@ function ShopReview() {
           <AppText size="15px" weight="SemiBold">
             방문자 리뷰
           </AppText>
-          <S.WriteButton>
+          <S.ActionButton>
+            <S.IconBox>
+              <CommentIcon />
+            </S.IconBox>
             <AppText>리뷰 쓰기</AppText>
-          </S.WriteButton>
+          </S.ActionButton>
         </S.TitleBox>
+        <S.ReviewList>
+          <S.defaultText>
+            <AppText color="#5A6068">작성된 후기가 없습니다.</AppText>
+            <AppText color="#5A6068">시술 후기를 작성 해보세요!</AppText>
+          </S.defaultText>
+        </S.ReviewList>
       </S.DetailReview>
+      {showScreen && <BriefReviewScreen navigation={navigation} />}
     </S.ReviewLayout>
   );
 }
