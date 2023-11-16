7d56d7017a3d1d2da0225ef2f0d1e29fc83f7fb4
JoEunsoo <dcpg95@gmail.com>
2023-11-17 08:05:49 +0900

    FEAT : 커뮤니티 커스텀 훅의 신고, 게시물 상세보기, 카테고리, 작성 로직 구현

--- src/hooks/community/communityPosts.hook.ts
+++ src/hooks/community/communityPosts.hook.ts
@@ -1,9 +1,13 @@
 import communityState from '@/recoil/community/community.recoil';
-import {useRecoilState} from 'recoil';
+import {useRecoilState, useRecoilValue} from 'recoil';
 
 export const useCommunityPosts = () => {
   const [state, setState] = useRecoilState(communityState);
-
+  /**
+   * 좋아요 버튼을 관리하는 로직
+   * @param id
+   * 인자로 받은 id 값을 recoil로 보내어 상태관리 함
+   */
   const likeButtonHandle = (id: number) => {
     setState(prev => ({
       ...prev,
@@ -11,12 +15,83 @@ export const useCommunityPosts = () => {
     }));
   };
 
-  const dropdownBackgroundHandle = () => {
+  const reportButtonHandle = (postId: number) => {
     setState(prev => ({
       ...prev,
-      showReportDropdown: false,
+      showReportDropdown: postId,
     }));
   };
 
-  return {likeButtonHandle, dropdownBackgroundHandle};
+  const dropdownBackgroundHandle = () => {
+    setState(prev => ({
+      ...prev,
+      showReportDropdown: null,
+    }));
+  };
+
+  const detailPostData = (
+    id: number,
+    description: string,
+    nickname: string,
+    title: string,
+    viewCount: number,
+  ) => {
+    setState(prev => ({
+      ...prev,
+      detailPostApi: {
+        id,
+        description,
+        nickname,
+        title,
+        viewCount,
+      },
+    }));
+  };
+
+  const postCategoryHandle = (
+    category: string,
+    key: 'postCategory' | 'serviceCategory',
+  ) => {
+    console.log('받아옴 : ' + category, key);
+    setState(prev => {
+      const isTagIncluded = prev.selectedPostCategory[key].some(
+        item => item === category,
+      );
+      const updatedSelectedTags = isTagIncluded
+        ? prev.selectedPostCategory[key].filter(item => item !== category)
+        : {
+            ...prev.selectedPostCategory[key],
+            [key]: [...prev.selectedPostCategory[key], category],
+          };
+      console.log(updatedSelectedTags);
+      /** tag 상태 값이 없는경우 버튼 비활성화 */
+      return {
+        ...prev,
+        selectedPostCategory: updatedSelectedTags,
+      };
+    });
+  };
+
+  const postInputHandle = (
+    data: string,
+    key: 'title' | 'description' | 'postCategory' | 'serviceCategory',
+  ) => {
+    console.log(state.selectedPostCategory);
+    setState(prev => ({
+      ...prev,
+      createPostData: {
+        ...prev.createPostData,
+        [key]: data,
+      },
+    }));
+  };
+
+  return {
+    likeButtonHandle,
+    reportButtonHandle,
+    dropdownBackgroundHandle,
+    detailPostData,
+    postCategoryHandle,
+    postInputHandle,
+  };
 };
