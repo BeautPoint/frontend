f9a7fb83dc429bdbe70a53b9df0ae7bc3da19aba
JoEunsoo <dcpg95@gmail.com>
2023-11-19 14:05:20 +0900

    REFACTOR : 드래그 로직 및 스프링 애니메이션 로직 개선

--- src/hooks/shop/bottomsheet.hook.ts
+++ src/hooks/shop/bottomsheet.hook.ts
@@ -1,5 +1,5 @@
 import shopState from '@/recoil/shop/shop.recoil';
-import {MutableRefObject, useRef, useState} from 'react';
+import {MutableRefObject} from 'react';
 import {Animated, PanResponder} from 'react-native';
 import {useSetRecoilState, useRecoilValue} from 'recoil';
 
@@ -25,49 +25,59 @@ export const useBottomSheetHook = () => {
     setShopInfo(prev => ({...prev, isButtonPressed: isPressed}));
   };
 
-  const dragHandle = ({height, translationY, panY}: DragHandleParams) => {
-    const dragStandardHeight = height * 0.26;
+  const springAnimation = (
+    direction: 'up' | 'down',
+    panY: Animated.Value,
+    height: number,
+    translationY: MutableRefObject<number>,
+  ) => {
     const maxHeight = height * 0.7;
     const minHeight = height * 0.1;
     const maximumTranslateY = minHeight - maxHeight;
     const minimumTranslateY = 0;
+
+    translationY.current =
+      direction === 'down' ? minimumTranslateY : maximumTranslateY;
+
+    Animated.spring(panY, {
+      toValue: translationY.current,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const dragHandle = ({height, translationY, panY}: DragHandleParams) => {
+    const dragStandardHeight = height * 0.26;
+
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
 
       onPanResponderGrant: () => {
-        panY.setOffset(translationY.current);
+        // panY.setOffset(translationY.current);
       },
       onPanResponderMove: (evt, gesture) => {
         panY.setValue(gesture.dy);
+
         console.log(-dragStandardHeight);
       },
       onPanResponderRelease: (evt, gesture) => {
         panY.flattenOffset();
 
-        if (gesture.dy > dragStandardHeight) {
-          springAnimation('down');
+        console.log(gesture.dy > -dragStandardHeight);
+        console.log('gesture : ' + gesture.dy);
+        console.log('dragStandardHeight : ' + -dragStandardHeight);
+
+        if (gesture.dy > -dragStandardHeight) {
+          springAnimation('down', panY, height, translationY);
           previewHandle(false);
-          // goBackButtonHandle(true);
+          goBackButtonHandle(true);
           console.log('내려감');
-        } else {
-          springAnimation('up');
+        } else if (gesture.dy < -dragStandardHeight) {
+          springAnimation('up', panY, height, translationY);
           previewHandle(true);
-          // goBackButtonHandle(false);
+          goBackButtonHandle(false);
         }
       },
     });
-
-    const springAnimation = (direction: 'up' | 'down') => {
-      console.log('direction', direction);
-      translationY.current =
-        direction === 'down' || shopInfo.isButtonPressed
-          ? minimumTranslateY
-          : maximumTranslateY;
-      Animated.spring(panY, {
-        toValue: translationY.current,
-        useNativeDriver: true,
-      }).start();
-    };
 
     return panResponder;
   };
