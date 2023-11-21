71bf041396520fbea62abac00f29feaaaa6c53d1
JoEunsoo <dcpg95@gmail.com>
2023-11-21 15:19:44 +0900

    ADD : 커뮤니티 recoil 카테고리, 게시물 상세보기, 게시물 쓰기 상태관리 추가

--- src/recoil/community/community.recoil.ts
+++ src/recoil/community/community.recoil.ts
@@ -12,7 +12,36 @@ const communityState = atom({
     ],
     selectedTab: '전체보기',
     likeButton: '' as string | number,
-    showReportDropdown: false,
+    showReportDropdown: null as number | null,
+    detailPostApi: {
+      id: 0,
+      description: '',
+      nickname: '',
+      title: '',
+      viewCount: 0,
+    },
+    postCategoies: [
+      {id: 0, postCategory: '정보'},
+      {id: 1, postCategory: '고민'},
+      {id: 2, postCategory: '자유게시글'},
+    ],
+    serviceCategoies: [
+      {id: 3, serviceCategory: 'SMP두피'},
+      {id: 4, serviceCategory: '이마라인'},
+      {id: 5, serviceCategory: '아이라인'},
+      {id: 6, serviceCategory: '눈썹'},
+      {id: 7, serviceCategory: '입술'},
+      {id: 8, serviceCategory: '흉터'},
+      {id: 9, serviceCategory: '반영구제거'},
+      {id: 10, serviceCategory: '기타'},
+    ],
+    selectedPostCategory: {postCategory: [], serviceCategory: []},
+    createPostData: {
+      title: '',
+      description: '',
+      postCategory: [],
+      serviceCategory: [],
+    },
   },
 });
 
