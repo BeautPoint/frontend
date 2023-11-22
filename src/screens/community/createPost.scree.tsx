08512c1fa45039036d6535069b2c6931fbe2ccbf
JoEunsoo <dcpg95@gmail.com>
2023-11-22 15:23:22 +0900

    DELETE : 파일명 변경으로 삭제

--- src/screens/community/createPost.scree.tsx
+++
@@ -1,8 +0,0 @@
-import * as S from '@/styles/community/create/createPost.style';
-import {NavigationProps} from '@/types/stackprops';
-
-function CreatePostScreen({navigation}: NavigationProps['community']) {
-  return <S.CreatePostLayout />;
-}
-
-export default CreatePostScreen;
