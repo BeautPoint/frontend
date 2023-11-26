23562a7684cbe729b6ffad0fec48f73497f39baa
JoEunsoo <dcpg95@gmail.com>
2023-11-26 16:27:32 +0900

    DESIGN : navigation header styled 구현

---
+++ src/styles/common/navigationHeader.style.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+const HeaderLayout = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
+  background: #ffffff;
+`;
+
+const Title = styled.View``;
+
+const BackButton = styled.Pressable``;
+
+const SubmitButton = styled.Pressable``;
+
+export {HeaderLayout, Title, BackButton, SubmitButton};
