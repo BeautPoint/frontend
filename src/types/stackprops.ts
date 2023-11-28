58b71e9122b616fcbac96fc6aaf8f921fec4f642
JoEunsoo <dcpg95@gmail.com>
2023-11-28 16:46:33 +0900

    ADD : navigation 경로 추가로 RootStackParamList, NavigationProps 확장

--- src/types/stackprops.ts
+++ src/types/stackprops.ts
@@ -1,6 +1,6 @@
 import {StackNavigationProp} from '@react-navigation/stack';
 
-type RootStackParamList = {
+export type RootStackParamList = {
   Home: undefined;
   Details: undefined;
   Login: undefined;
@@ -10,19 +10,46 @@ type RootStackParamList = {
   ShopDetails: undefined;
   SignUp: undefined;
   Location: undefined;
+  Community: undefined;
+  HomeSearch: undefined;
+  LocaitSearch: undefined;
+  CreatePost: undefined;
+  BriefReiview: undefined;
+  ShopReview: undefined;
 };
 
 type LoginScreenProps = StackNavigationProp<RootStackParamList, 'Login'>;
+type HomeScreenProps = StackNavigationProp<RootStackParamList, 'HomeSearch'>;
 type SignUpScreenProps = StackNavigationProp<RootStackParamList, 'SignUp'>;
 type ConfirmModalProps = StackNavigationProp<RootStackParamList, 'Confirm'>;
 type LocationScreenProps = StackNavigationProp<
   RootStackParamList,
-  'Search' | 'ShopDetails'
+  'LocaitSearch'
+>;
+type CommunityScreenProps = StackNavigationProp<
+  RootStackParamList,
+  'Community'
+>;
+type ShopDetailScreenProps = StackNavigationProp<
+  RootStackParamList,
+  'ShopDetails'
+>;
+type CreatePostScreenProps = StackNavigationProp<
+  RootStackParamList,
+  'CreatePost'
+>;
+type ShopReviewScreenProps = StackNavigationProp<
+  RootStackParamList,
+  'ShopReview'
 >;
 
 export interface NavigationProps {
   login: {navigation: LoginScreenProps};
+  home: {navigation: HomeScreenProps};
   confirm: {navigation: ConfirmModalProps};
   signup: {navigation: SignUpScreenProps};
   location: {navigation: LocationScreenProps};
+  community: {navigation: CommunityScreenProps};
+  shopDetails: {navigation: ShopDetailScreenProps};
+  shopReview: {navigation: ShopReviewScreenProps};
 }
