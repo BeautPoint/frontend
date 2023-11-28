cd9359cd27838f233484fb2cb3cafe8e82b54186
JoEunsoo <dcpg95@gmail.com>
2023-11-28 16:41:42 +0900

    DESIGN : 사업장 상세보기 화면의 styled 구현

---
+++ src/styles/screens/shopinfo.style.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+
+interface PropsType {
+  dragBottomSheet: {height: number};
+}
+
+const DetailLayOut = styled.ScrollView`
+  width: 100%;
+  height: 100%;
+`;
+
+const ScrollView = styled.Pressable`
+  width: 100%;
+  /* padding-bottom: 40px; */
+  background: #f4f5f6;
+`;
+
+const DragDownBottomSheet = styled.View<PropsType['dragBottomSheet']>`
+  width: 100%;
+  height: ${({height}) => height}px;
+  position: absolute;
+  /* bottom: 0;
+  right: 0; */
+  top: 10px;
+  z-index: 2;
+`;
+
+const BackButton = styled.Pressable`
+  position: absolute;
+  z-index: 2;
+  left: 20px;
+  top: 20px;
+`;
+
+export {DetailLayOut, ScrollView, BackButton, DragDownBottomSheet};
