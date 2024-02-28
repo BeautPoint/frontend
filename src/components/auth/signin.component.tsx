848d8d2b01aaf3fdcbbfc8de68fd1056ecfec726
JoEunsoo <dcpg95@gmail.com>
2024-02-28 18:05:26 +0900

    REFACTOR : 로그인 컴포넌트 로그인 완료시 홈화면 이동 네비게이션 추가

--- src/components/auth/signin.component.tsx
+++ src/components/auth/signin.component.tsx
@@ -6,17 +6,28 @@ import NaverOauthButton from '@/components/auth/social/naverOauth.component';
 import * as S from '@/styles/auth/signin.style';
 import {AppText} from '@/styles/global.style';
 import {useNaverOauth} from '@/hooks/auth/oauth.hooks';
+import {NavigationProps} from '@/types/stackprops';
+import {useRecoilValue} from 'recoil';
+import navigationState from '@/recoil/navigation/navigation.recoil';
 
-function SignIn() {
+function SignIn({navigation}: NavigationProps['signup']) {
   const {logout} = useNaverOauth();
+  const {singupScreen, resetToHomeScreen} = useRecoilValue(navigationState);
+  console.log(singupScreen);
+  singupScreen ? navigation.navigate('SignUp') : null;
+  resetToHomeScreen ? navigation.reset({routes: [{name: 'Home'}]}) : null;
   return (
     <S.SignInLayOut>
-      <KakaoOauthButton />
-      <NaverOauthButton />
-      <Pressable onPress={() => logout()}>
+      <KakaoOauthButton navigation={navigation} />
+      <NaverOauthButton navigation={navigation} />
+      {/* <Pressable onPress={() => logout()}>
         <AppText>로그아웃</AppText>
-      </Pressable>
-      {Platform.OS === 'android' ? <GoogleOauthButton /> : <AppleOauthButton />}
+      </Pressable> */}
+      {Platform.OS === 'android' ? (
+        <GoogleOauthButton navigation={navigation} />
+      ) : (
+        <AppleOauthButton />
+      )}
     </S.SignInLayOut>
   );
 }
