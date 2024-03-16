5003f5a1fc164926df167ddd2963f7455de80ed1
JoEunsoo <dcpg95@gmail.com>
2024-03-16 16:41:39 +0900

    FEAT : 공통 UI 네비게이션 헤더 구현

---
+++ src/components/common/navHeader.component.tsx
@@ -0,0 +1,24 @@
+import * as S from '@/styles/common/navHeader.style';
+import BackIcon from '@/assets/icons/backIcon.svg';
+import {NavigationProps} from '@/types/stackprops';
+import {useRecoilValue} from 'recoil';
+import navigationState from '@/recoil/navigation/navigation.recoil';
+import {AppText} from '@/styles/global.style';
+
+function NavHeader({navigation}: NavigationProps['navigation']) {
+  const {headerTitle} = useRecoilValue(navigationState);
+  return (
+    <S.HeaderLayout>
+      <S.MainContent>
+        <S.BackButton onPress={() => navigation.goBack()}>
+          <BackIcon />
+        </S.BackButton>
+        <S.Title>
+          <AppText size="15px">{headerTitle}</AppText>
+        </S.Title>
+      </S.MainContent>
+    </S.HeaderLayout>
+  );
+}
+
+export default NavHeader;
