329732ab874801e7dd901034ffe58cc8b2504674
JoEunsoo <dcpg95@gmail.com>
2024-03-18 17:33:13 +0900

    FEAT : 마이페이지의 게시물 목록 화면 구현

---
+++ src/components/profile/userContentList.component.tsx
@@ -0,0 +1,75 @@
+import {AppText} from '@/styles/global.style';
+import * as S from '@/styles/profile/userContent.style';
+
+function UserContentList() {
+  return (
+    <S.UserContnetLayout>
+      <S.ContnetBox>
+        <S.Title>
+          <AppText>Title</AppText>
+        </S.Title>
+        <S.Description>
+          <AppText numberOfLines={2} size="13px">
+            자연스럽게 하는 곳 아시는 분 있나요? 아시는 분 있으면
+            소개부탁드릴게요 서울/경기 지역 입니다:)
+          </AppText>
+        </S.Description>
+        <S.CreatedDate>
+          <AppText size="12px" color="#9ea2a7">
+            2023.10.10
+          </AppText>
+        </S.CreatedDate>
+      </S.ContnetBox>
+      <S.ContnetBox>
+        <S.Title>
+          <AppText>Title</AppText>
+        </S.Title>
+        <S.Description>
+          <AppText numberOfLines={2} size="13px">
+            자연스럽게 하는 곳 아시는 분 있나요? 아시는 분 있으면
+            소개부탁드릴게요 서울/경기 지역 입니다:)
+          </AppText>
+        </S.Description>
+        <S.CreatedDate>
+          <AppText size="12px" color="#9ea2a7">
+            2023.10.10
+          </AppText>
+        </S.CreatedDate>
+      </S.ContnetBox>
+      <S.ContnetBox>
+        <S.Title>
+          <AppText>Title</AppText>
+        </S.Title>
+        <S.Description>
+          <AppText numberOfLines={2} size="13px">
+            자연스럽게 하는 곳 아시는 분 있나요? 아시는 분 있으면
+            소개부탁드릴게요 서울/경기 지역 입니다:)
+          </AppText>
+        </S.Description>
+        <S.CreatedDate>
+          <AppText size="12px" color="#9ea2a7">
+            2023.10.10
+          </AppText>
+        </S.CreatedDate>
+      </S.ContnetBox>
+      <S.ContnetBox>
+        <S.Title>
+          <AppText>Title</AppText>
+        </S.Title>
+        <S.Description>
+          <AppText numberOfLines={2} size="13px">
+            자연스럽게 하는 곳 아시는 분 있나요? 아시는 분 있으면
+            소개부탁드릴게요 서울/경기 지역 입니다:)
+          </AppText>
+        </S.Description>
+        <S.CreatedDate>
+          <AppText size="12px" color="#9ea2a7">
+            2023.10.10
+          </AppText>
+        </S.CreatedDate>
+      </S.ContnetBox>
+    </S.UserContnetLayout>
+  );
+}
+
+export default UserContentList;
