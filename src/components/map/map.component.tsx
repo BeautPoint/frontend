9593f0d30900920301fa8f87bae79a78a89b9be5
JoEunsoo <dcpg95@gmail.com>
2024-03-18 16:48:22 +0900

    REFACTOR : 구글 지도내 사업장 표시 되도록 데이터 및 마커 추가

--- src/components/map/map.component.tsx
+++ src/components/map/map.component.tsx
@@ -1,20 +1,25 @@
+import {useShopService} from '@/hooks/shop/shopService.hook';
 import {View} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
-function MapComponent() {
+function MapComponent({placesInfo}) {
   const GangNamgu = {
     latitude: 37.5212304114637,
     longitude: 127.022686095616,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
+  console.log('지도 : ', placesInfo);
+
+  const {handleShopDetails} = useShopService();
+
   return (
     <View
       style={{
         flex: 1,
       }}>
       <MapView
-        style={{width: '100%', height: '100%'}}
+        style={{flex: 1, width: '100%', height: '100%'}}
         provider={PROVIDER_GOOGLE}
         customMapStyle={[
           {
@@ -26,7 +31,17 @@ function MapComponent() {
           ...GangNamgu,
         }}>
         {/* <MapMarker /> */}
-        <Marker coordinate={GangNamgu} />
+        {placesInfo?.map((item, i) => {
+          console.log(item);
+          return (
+            <Marker
+              key={i}
+              coordinate={item?.location}
+              onPress={() => handleShopDetails(item)}
+            />
+          );
+        })}
+        {/* <Marker coordinate={GangNamgu} onPress={() => cons} /> */}
       </MapView>
     </View>
   );
