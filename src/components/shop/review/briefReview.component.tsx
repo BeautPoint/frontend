a37b49b45c52e80006fd529a2c3f69ea293f6343
JoEunsoo <dcpg95@gmail.com>
2024-03-22 17:39:18 +0900

    REFACTOR : 간편리뷰 화면의 리뷰 선택 recoil 상태값 추가

--- src/components/shop/review/briefReview.component.tsx
+++ src/components/shop/review/briefReview.component.tsx
@@ -1,15 +1,58 @@
 import NavigationHeader from '@/components/common/navigationHeader';
 import {NavigationProps} from '@/types/stackprops';
+import * as S from '@/styles/shop/detail/briefReview.style';
+import {AppText} from '@/styles/global.style';
+import {useRecoilValue} from 'recoil';
+import shopState from '@/recoil/shop/shop.recoil';
+import {Fragment} from 'react';
+import {useShopDetails} from '@/hooks/shop/shopDetail.hook';
+import CheckIcon from '@/assets/icons/shopDetail/checkBoxActived.svg';
+import NoneCheckIcon from '@/assets/icons/shopDetail/checkBoxNormal.svg';
 
 function BriefReviewScreen({navigation}: NavigationProps['shopDetails']) {
+  const {BriefReviewList, briefReviewData} = useRecoilValue(shopState);
+  const {BriefReviewCheckBoxHandle} = useShopDetails();
+  const reviewChecked = BriefReviewList.some(item => item.isSelected === true);
+  console.log(briefReviewData);
   return (
-    <>
+    <Fragment>
       <NavigationHeader
+        isActived={reviewChecked}
         navigation={navigation}
-        navType={'shopDetails'}
         title={'글쓰기'}
+        url="brief"
       />
-    </>
+      <S.BriefReviewLayout>
+        <S.TitleSection>
+          <S.ReviewTitle>
+            <AppText size="18px" weight="Semibold">
+              해당 샵의 좋았던 점은 무엇인가요?
+            </AppText>
+          </S.ReviewTitle>
+          <S.SubTitle>
+            <AppText color="#717984">복수 선택이 가능해요</AppText>
+          </S.SubTitle>
+        </S.TitleSection>
+        <S.SelectedList>
+          {BriefReviewList.map(item => (
+            <S.CheckBoxButton
+              key={item.id}
+              onPress={() =>
+                BriefReviewCheckBoxHandle(item.id, item.description)
+              }>
+              <S.ButtonIcon>
+                {item.isSelected ? (
+                  <CheckIcon color="#619BFF" strokeWidth="1" />
+                ) : (
+                  <NoneCheckIcon />
+                )}
+              </S.ButtonIcon>
+              <AppText>{item.description}</AppText>
+            </S.CheckBoxButton>
+          ))}
+        </S.SelectedList>
+      </S.BriefReviewLayout>
+    </Fragment>
   );
 }
 export default BriefReviewScreen;
