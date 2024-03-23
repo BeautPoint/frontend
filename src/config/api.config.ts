e1aba421f591a87b9675ec6b497737cdbc6e7d24
JoEunsoo <dcpg95@gmail.com>
2024-03-23 17:41:37 +0900

    CHORE : api 요청 url 설정 수정

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -3,7 +3,36 @@ import {Platform} from 'react-native';
 
 const url = Platform.OS === 'android' ? 'http://10.0.2.2' : 'http://localhost';
 
-export const PostAPI = axios.create({
+const ServerApi = axios.create({
   //   baseURL: process.env.AUTH_API,
-  baseURL: `${url}:3005/community`,
+  baseURL: `${url}:3005/`,
 });
+
+const PostAPI = axios.create({
+  //   baseURL: process.env.AUTH_API,
+  baseURL: `${url}:3005/community/post`,
+  headers: {
+    Accept: 'application/json',
+    'content-Type': 'application/json',
+  },
+});
+
+const AuthAPI = axios.create({
+  //   baseURL: process.env.AUTH_API,
+  baseURL: `${url}:3005/auth`,
+  headers: {
+    Accept: 'application/json',
+    'content-Type': 'application/json',
+  },
+});
+
+const UserAPI = axios.create({
+  //   baseURL: process.env.AUTH_API,
+  baseURL: `${url}:3005/user`,
+  headers: {
+    Accept: 'application/json',
+    'content-Type': 'application/json',
+  },
+});
+
+export {PostAPI, AuthAPI, ServerApi, UserAPI};
