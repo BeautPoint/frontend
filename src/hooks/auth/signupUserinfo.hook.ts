8e0efac83aeeb16be461c175a82398b8f4a42484
JoEunsoo <dcpg95@gmail.com>
2024-03-25 17:47:04 +0900

    REFACOTR : 회원가입 커스텀 훅의 api 요청 로직과 생년월일 데이터 값 받아오는 로직 추가

--- src/hooks/auth/signupUserinfo.hook.ts
+++ src/hooks/auth/signupUserinfo.hook.ts
@@ -1,13 +1,20 @@
+import {useAuthQuery} from '@/api/auth/auth.api';
 import signupState from '@/recoil/auth/signupInfoForm.recoil';
+import confirmModalState from '@/recoil/modal/confirm.recoil';
+import navigationState from '@/recoil/navigation/navigation.recoil';
 import {useEffect, useState} from 'react';
-import {useRecoilValue, useSetRecoilState} from 'recoil';
+import {useRecoilValue, useSetRecoilState, useRecoilState} from 'recoil';
 
 const useSignupUserInfo = () => {
   const setUserInfoForm = useSetRecoilState(signupState);
+  const [navState, setNavState] = useRecoilState(navigationState);
   const userInfoForm = useRecoilValue(signupState);
+  const confirmForm = useRecoilValue(confirmModalState);
+  const [signupUserState, setState] = useRecoilState(signupState);
   const [userBirth, setUserBirth] = useState<Date>();
   const [isActive, setIsActive] = useState(false);
   const [userGender, setUserGender] = useState<string | null>(null);
+  const {signupApi} = useAuthQuery();
 
   useEffect(() => {
     if (userGender && userBirth) {
@@ -30,7 +37,8 @@ const useSignupUserInfo = () => {
     const userMonth = new Date(date).getMonth() + 1;
     const userDate = new Date(date).getDate();
     setIsActive(true);
-
+    const birthDate = new Date(userYear, userMonth, userDate);
+    console.log(userInfoForm.birthDate);
     setUserInfoForm(prevState => ({
       ...prevState,
       birthInfo: prevState.birthInfo.map(item => {
@@ -49,16 +57,31 @@ const useSignupUserInfo = () => {
         return {...item, dateValue};
       }),
     }));
+    console.log('생일 :' + birthDate);
     setUserBirth(new Date(userYear, userMonth - 1, userDate + 1));
   };
 
-  const nextButtonHandle = () => {
+  const infoCollectionHandle = (progressStep: number) => {
+    const signupUserInfo = {birthDate: userBirth, gender: userGender};
+    console.log(signupUserInfo);
+    if (progressStep < 3) {
+      const updateStep = progressStep + 1;
+
+      setUserInfoForm(prevState => ({
+        ...prevState,
+        nextButtonActive: false,
+        progressStep: updateStep,
+      }));
+    }
     setUserInfoForm(prev => {
       if (prev.selectedTags.length) {
         return {...prev, nextButtonActive: true};
       }
       return {...prev, nextButtonActive: false};
     });
+    if (progressStep === 3) {
+      // signupApi(signupUserInfo);
+    }
   };
 
   interface TagProps {
@@ -86,12 +109,58 @@ const useSignupUserInfo = () => {
     });
   };
 
+  const getUserSignupData = (
+    key: 'birthDate' | 'gender',
+    userData: Date | string,
+  ) => {
+    setUserInfoForm(prevState => {
+      const updatedInfo = {
+        ...prevState.signupUserInfo,
+        [key]: userData,
+      };
+
+      return {...prevState, signupUserInfo: updatedInfo};
+    });
+
+    console.log('유저 : ', userInfoForm.signupUserInfo);
+  };
+
+  const handleSubmit = () => {
+    // step < 3 ? setStep(step + 1) : navigation.reset({routes: [{name: 'Home'}]});
+    const stepNumber = signupUserState.progressStep;
+    infoCollectionHandle(stepNumber);
+    setNavState(prevState => {
+      const updateState = navState.singupScreen
+        ? false
+        : signupUserState.progressStep === 3;
+      return {
+        ...prevState,
+        resetToHomeScreen: signupUserState.progressStep === 3,
+      };
+    });
+
+    const optionalConsents = confirmForm.optionalConsents;
+
+    const userData = {
+      ...userInfoForm.signupUserInfo,
+      userConsents: 'Y',
+      optionalConsents,
+    };
+
+    if (stepNumber === 3) {
+      return signupApi(userData);
+    }
+  };
+
   return {
     datePickerHandle,
     userBirth,
     isActive,
     getUserGender,
     tagSelectionHandle,
+    infoCollectionHandle,
+    getUserSignupData,
+    handleSubmit,
   };
 };
 
