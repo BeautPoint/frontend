392c5e30fa7979b9cb3a00871f310e67df0876f6
JoEunsoo <dcpg95@gmail.com>
2024-03-31 22:37:16 +0900

    REFACTOR : 사업장 간편리뷰 recoil 추가

--- src/recoil/shop/shop.recoil.ts
+++ src/recoil/shop/shop.recoil.ts
@@ -9,13 +9,35 @@ const shopState = atom({
     previewFullScreen: false,
     isButtonPressed: false,
     BriefReviewList: [
-      {id: 0, description: '친절하고 서비스가 좋아요'},
-      {id: 1, description: '분위기가 편한하고 좋아요'},
-      {id: 2, description: '깨끗하고 위생이 좋아요'},
-      {id: 3, description: '인테리어와 시설이 좋아요'},
-      {id: 4, description: '가성비가 좋아요'},
+      {
+        id: 'brief-0',
+        description: '친절하고 서비스가 좋아요',
+        isSelected: false,
+      },
+      {
+        id: 'brief-1',
+        description: '분위기가 편한하고 좋아요',
+        isSelected: false,
+      },
+      {id: 'brief-2', description: '깨끗하고 위생이 좋아요', isSelected: false},
+      {
+        id: 'brief-3',
+        description: '인테리어와 시설이 좋아요',
+        isSelected: false,
+      },
+      {id: 'brief-4', description: '가성비가 좋아요', isSelected: false},
     ],
+    briefReviewData: [] as {reviewId: ''; description: ''}[],
     navigate: () => {},
+    reviewRating: [
+      {id: 0, rating: 1, isActived: false},
+      {id: 1, rating: 2, isActived: false},
+      {id: 2, rating: 3, isActived: false},
+      {id: 3, rating: 4, isActived: false},
+      {id: 4, rating: 5, isActived: false},
+    ],
+    detailReviewData: {rating: 0, textReview: ''},
+    selectedShop: {name: '', address: '', photo: ''},
   },
 });
 
