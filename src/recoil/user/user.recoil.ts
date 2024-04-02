b5ddd88734741c393cfee87faddbf65ad7337d79
JoEunsoo <dcpg95@gmail.com>
2024-04-02 23:42:32 +0900

    FEAT : 사용자 프로필 관리를 위해 userInfo 상태 recoil 구현

---
+++ src/recoil/user/user.recoil.ts
@@ -0,0 +1,11 @@
+import {atom} from 'recoil';
+import {randomString} from '@/utils/randomString.util';
+const userInfoState = atom({
+  key: `userInfoState/${randomString()}`,
+  default: {
+    userProfile: {nickName: '', profile_image: '', socialType: ''},
+    
+  },
+});
+
+export default userInfoState;
