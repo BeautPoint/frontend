1f61c65fc1c3d381fdb279add29c1a091e23fcff
JoEunsoo <dcpg95@gmail.com>
2024-04-04 00:00:30 +0900

    REFACOTR : 커뮤니티 게시물 작성시 사용자 인증 로직 추가

--- src/screens/main/community.screen.tsx
+++ src/screens/main/community.screen.tsx
@@ -7,18 +7,27 @@ import {useRecoilValue} from 'recoil';
 import communityState from '@/recoil/community/community.recoil';
 import {useCommunityPosts} from '@/hooks/community/communityPosts.hook';
 import CreatePostScreen from '../community/createPost.screen';
+import {useAuthHook} from '@/hooks/auth/auth.hook';
 
 function CommunityScreen({navigation}: NavigationProps['community']) {
   const {showReportDropdown} = useRecoilValue(communityState);
   const {dropdownBackgroundHandle} = useCommunityPosts();
   const showShopDetail = false;
+  const {accessToken} = useAuthHook();
+
+  const handleCreatePost = () => {
+    accessToken
+      ? navigation.navigate('CreatePost')
+      : navigation.navigate('Login');
+  };
+
   return (
     <S.CommunityLayOut>
       <CommunityHeader />
       <S.ScrollViewBox>
         <CommunityPost navigation={navigation} />
       </S.ScrollViewBox>
-      <S.NewPostButton onPress={() => navigation.navigate('CreatePost')}>
+      <S.NewPostButton onPress={() => handleCreatePost()}>
         <WriteIcon color="#ffffff" />
       </S.NewPostButton>
       {showReportDropdown ? (
