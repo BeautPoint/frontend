6b9eb2424de6a2d691cd186790a188ff128064b7
JoEunsoo <dcpg95@gmail.com>
2024-04-17 00:10:27 +0900

    DESIGN : 사업장 간편리뷰 컴포넌트 styled 구현

--- src/styles/shop/detail/briefReview.style.tsx
+++ src/styles/shop/detail/briefReview.style.tsx
@@ -1,5 +1,40 @@
 import styled from 'styled-components/native';
 
-const BriefReviewLayout = styled.View``;
+const BriefReviewLayout = styled.View`
+  padding: 20px;
+  background: #ffffff;
+  height: 100%;
+`;
 
-export {BriefReviewLayout};
+const TitleSection = styled.View`
+  margin-bottom: 30px;
+`;
+
+const ReviewTitle = styled.View`
+  margin-bottom: 10px;
+`;
+
+const SubTitle = styled.View``;
+
+const SelectedList = styled.View``;
+
+const CheckBoxButton = styled.Pressable`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+  align-items: center;
+`;
+
+const ButtonIcon = styled.View`
+  margin-right: 15px;
+`;
+
+export {
+  BriefReviewLayout,
+  TitleSection,
+  ReviewTitle,
+  SubTitle,
+  SelectedList,
+  CheckBoxButton,
+  ButtonIcon,
+};
