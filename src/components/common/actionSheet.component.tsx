3dbebc3e125f154ee15262e6eb0a9d82f128b292
JoEunsoo <dcpg95@gmail.com>
2024-05-13 23:55:43 +0900

    FEAT : 앱 액션 시트 ui 구현

---
+++ src/components/common/actionSheet.component.tsx
@@ -0,0 +1,47 @@
+import {useActionSheetHook} from '@/hooks/common/actionSheet.hooks';
+import actionSheetState from '@/recoil/modal/actionSheet.recoil';
+import * as S from '@/styles/common/actionSheet.style';
+import {AppText} from '@/styles/global.style';
+import {useState} from 'react';
+import {Alert} from 'react-native';
+import {useRecoilValue} from 'recoil';
+
+function ActionSheet() {
+  const [isPressed, setIsPressed] = useState<number | null>(null);
+
+  const {menuList, isOpen, isPressedMenu} = useRecoilValue(actionSheetState);
+  const {handleMenuSelection, openActionSheet} = useActionSheetHook();
+  const handleOnPress = (pressEvent: () => void | null) => {
+    if (pressEvent) pressEvent();
+    return openActionSheet(false);
+  };
+
+  return (
+    <S.Layout display={isOpen}>
+      <S.Overlay onPress={() => openActionSheet(false)}>
+        {menuList.map(item => {
+          return (
+            <S.ActionButton
+              key={item.id}
+              onPressIn={() => handleMenuSelection(item.id)}
+              onPressOut={() => handleMenuSelection(null)}
+              onPress={() => handleOnPress(item.pressEvent)}
+              hover={isPressedMenu === item.id}
+              background={item.id === 0 ? '#ffffff' : 'rgba(255,255,255,0.84)'}
+              borderRadius={item.id}
+              marginTop={item.id}>
+              <AppText
+                color={item.id === 2 ? '#FD4E44' : '#004bec'}
+                weight={item.id === 0 ? 'semiBold' : ''}
+                size="18px">
+                {item.title}
+              </AppText>
+            </S.ActionButton>
+          );
+        })}
+      </S.Overlay>
+    </S.Layout>
+  );
+}
+
+export default ActionSheet;
