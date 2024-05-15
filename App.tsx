efb13cb0012c2ebed0ef9db9fb540438046ced20
JoEunsoo <dcpg95@gmail.com>
2024-05-15 23:58:28 +0900

    ADD : action sheet ui 추가

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import {RecoilRoot} from 'recoil';
 import GlobalModals from '@/components/modals/globalmodal.component';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+import ActionSheet from '@/components/common/actionSheet.component';
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ function App() {
             <StackNavigation />
           </Container>
           <GlobalModals />
+          <ActionSheet />
         </NavigationContainer>
       </RecoilRoot>
     </QueryClientProvider>
