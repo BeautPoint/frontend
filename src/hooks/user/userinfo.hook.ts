b35015b829cc78b7505e70ba865b85e0b07d9fb4
JoEunsoo <dcpg95@gmail.com>
2024-05-17 23:59:59 +0900

    ADD : 회원정보 수정 비즈니스 로직 구현 및 api 추가

--- src/hooks/user/userinfo.hook.ts
+++ src/hooks/user/userinfo.hook.ts
@@ -1,6 +1,10 @@
 import {useRecoilState, useSetRecoilState} from 'recoil';
 import {useState, useEffect} from 'react';
-import {getUserProfile} from '@/api/user/userInfo.api';
+import {
+  editUserProfile,
+  getUserProfile,
+  updateProfilePhoto,
+} from '@/api/user/userInfo.api';
 import userInfoState from '@/recoil/user/user.recoil';
 import {processErrorHandler} from '@/middleware/errorHandler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -47,5 +51,45 @@ export const useUserInfoHook = () => {
     }
   };
 
-  return {fetchUserProfile};
+  const updatedUserProfile = (data: any) => {
+    try {
+    } catch {}
+  };
+
+  const changeDefaultprofilePhoto = async () => {
+    setUserProfile(prevState => ({
+      ...prevState,
+      userProfile: {...prevState.userProfile, profile_image: ''},
+    }));
+    const imageData = {profile_image: null};
+    try {
+      const result = await editUserProfile(imageData);
+      console.log(result);
+    } catch (err: any) {
+      console.log('유저 에러 :', err.response.data);
+    }
+  };
+
+  const changeProfilePhoto = async (imageData: any) => {
+    const formData = new FormData();
+    setUserProfile(prevState => ({
+      ...prevState,
+      userProfile: {...prevState.userProfile, profile_image: imageData.uri},
+    }));
+    formData.append('imageFile', imageData);
+    console.log('imageData hook : ', formData);
+    try {
+      const result = await updateProfilePhoto(formData);
+      console.log(result);
+    } catch (err: any) {
+      console.log('유저 에러 :', err.response.data);
+    }
+  };
+
+  return {
+    fetchUserProfile,
+    changeProfilePhoto,
+    changeDefaultprofilePhoto,
+    updatedUserProfile,
+  };
 };
