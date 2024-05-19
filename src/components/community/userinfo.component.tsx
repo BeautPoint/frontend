120236feb7c4bce3eb210b7d2f35dc177f4c35a8
JoEunsoo <dcpg95@gmail.com>
2024-05-19 18:08:46 +0900

    FEAT : 커뮤니티 유저 정보 컴포넌트 기능 개선 - 날짜 표시, 게시물 상세 화면 편집 기능 추가

--- src/components/community/userinfo.component.tsx
+++ src/components/community/userinfo.component.tsx
@@ -5,34 +5,54 @@ import DropDown from '@/components/common/dropdown.component';
 import {useRecoilValue} from 'recoil';
 import communityState from '@/recoil/community/community.recoil';
 import {useCommunityPosts} from '@/hooks/community/communityPosts.hook';
+import DefaultProfile from '@/assets/images/defaultProfile.png';
+import {dateCalculator} from '@/utils/dateCalculator.util';
+import {useActionSheetHook} from '@/hooks/common/actionSheet.hooks';
 
-function CommunityUserInfo({postId, nickname, viewCount}) {
-  const {showReportDropdown} = useRecoilValue(communityState);
+function CommunityUserInfo({
+  postId,
+  nickname,
+  viewCount,
+  profileImage,
+  createdAt,
+}) {
+  const {showReportDropdown, userPosts, isDetailScreen} =
+    useRecoilValue(communityState);
+  const {updateMenuList, openActionSheet} = useActionSheetHook();
   const {reportButtonHandle} = useCommunityPosts();
+  const formattedDate = dateCalculator(createdAt);
+
+  const handleEditPost = (postId: string) => {
+    const selectedPost = userPosts.filter(item => {
+      return item.post_id === postId;
+    });
+
+    selectedPost[0] ? updateMenuList('post') : updateMenuList('report');
+    openActionSheet(true);
+  };
   return (
     <S.UserInfoLayout>
-      <S.ProfileImage />
+      <S.ProfilePhoto
+        source={profileImage ? {uri: profileImage} : DefaultProfile}
+      />
       <S.InfoBox>
         <S.UserName>
           <AppText>{nickname}</AppText>
         </S.UserName>
         <S.PostStats>
           <S.CreatedAt>
-            <AppText size="12px">3분 전</AppText>
+            <AppText size="12px">{formattedDate}</AppText>
           </S.CreatedAt>
           <S.Views>
             <AppText size="12px">조회수 {viewCount}</AppText>
           </S.Views>
         </S.PostStats>
       </S.InfoBox>
-      <S.ReportButton onPress={() => reportButtonHandle(postId)}>
-        <DotsIcon />
-      </S.ReportButton>
-      {showReportDropdown === postId ? (
-        <S.ReportDropdown>
-          <DropDown dropDownHeight={'60px'} />
-        </S.ReportDropdown>
-      ) : null}
+      {isDetailScreen && (
+        <S.EditPostButton onPress={() => handleEditPost(postId)}>
+          <DotsIcon />
+        </S.EditPostButton>
+      )}
     </S.UserInfoLayout>
   );
 }
