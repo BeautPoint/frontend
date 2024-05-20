803ef18714d3eb81f730f2b8f6e5b7ee8c463864
JoEunsoo <dcpg95@gmail.com>
2024-05-20 22:00:25 +0900

    FEAT : 액션시트 커스텀 훅 메뉴별 실행 로직 구현

--- src/hooks/common/actionSheet.hooks.tsx
+++ src/hooks/common/actionSheet.hooks.tsx
@@ -1,11 +1,27 @@
 import actionSheetState from '@/recoil/modal/actionSheet.recoil';
 import {leanchSelectImage} from '@/utils/launchImage.util';
 import {useSetRecoilState} from 'recoil';
+import {useCommunityPosts} from '../community/communityPosts.hook';
 import {useUserInfoHook} from '../user/userinfo.hook';
 
 export const useActionSheetHook = () => {
   const setActionSheet = useSetRecoilState(actionSheetState);
   const {changeProfilePhoto, changeDefaultprofilePhoto} = useUserInfoHook();
+  const {handleIsEditMode} = useCommunityPosts();
+  const handleMenuSelection = (id: number | null) => {
+    return setActionSheet(prevState => ({
+      ...prevState,
+      isPressedMenu: id,
+    }));
+  };
+
+  const openActionSheet = (isOpen: boolean) => {
+    setActionSheet(prevState => ({
+      ...prevState,
+      isOpen,
+    }));
+  };
+
   const updateMenuList = (menuType: string) => {
     let menuItems: any;
     if (menuType === 'image') {
@@ -24,23 +40,36 @@ export const useActionSheetHook = () => {
       ];
     }
 
-    setActionSheet(prevState => ({
-      ...prevState,
-      menuList: menuItems,
-    }));
-  };
+    if (menuType === 'report') {
+      menuItems = [
+        {
+          id: 9,
+          title: '신고하기',
+          pressEvent: () => console.log('삭제'),
+        },
+        {id: 0, title: '취소', pressEvent: null},
+      ];
+    }
 
-  const handleMenuSelection = (id: number | null) => {
+    if (menuType === 'post') {
+      menuItems = [
+        {
+          id: 1,
+          title: '게시물 수정하기',
+          pressEvent: () => handleIsEditMode(true),
+        },
+        {
+          id: 2,
+          title: '게시물 삭제하기',
+          pressEvent: () => console.log('삭제'),
+        },
+        {id: 0, title: '취소', pressEvent: null},
+      ];
+    }
+
     return setActionSheet(prevState => ({
       ...prevState,
-      isPressedMenu: id,
-    }));
-  };
-
-  const openActionSheet = (isOpen: boolean) => {
-    setActionSheet(prevState => ({
-      ...prevState,
-      isOpen,
+      menuList: menuItems,
     }));
   };
 
