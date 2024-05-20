ae690f609416c566f043888cb920f0bdacff55a7
JoEunsoo <dcpg95@gmail.com>
2024-05-21 02:44:41 +0900

    FEAT : 예외처리 미들웨어 토큰 재발급 및 기간만료시 토큰 삭제 로직 구현

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,9 +1,30 @@
-import {reissueToken} from '@/api/auth/auth.api';
 import {getUserProfile} from '@/api/user/userInfo.api';
+import {useAuthHook} from '@/hooks/auth/auth.hook';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {NavigationProps} from '@/types/stackprops';
 
-export const processErrorHandler = async (error: any) => {
-  const status = error.statusCode;
-  if (status === 401) {
-    return await reissueToken();
-  }
+export const useProcessError = () => {
+  const handleError = async (err: any) => {
+    const status = err.statusCode;
+    const message = err.message;
+    const fallback = err.fallback;
+
+    if (status === 401) {
+      const reissueSuccess = await fallback.reissueToken();
+      console.log('reissueSuccess : ', reissueSuccess);
+      console.log('fallback : ', fallback);
+      return reissueSuccess;
+    }
+
+    if (message === 'refreshToken is not found') {
+      console.log('fallback : ', fallback);
+      const removeSuccess = fallback.removeToken();
+      console.log('removeSuccess : ', removeSuccess);
+      return removeSuccess;
+    }
+
+    return;
+  };
+
+  return {handleError};
 };
