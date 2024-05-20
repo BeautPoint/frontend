9c9c7b7835f29195a868bf9f1dcf2c8a3d0350ea
JoEunsoo <dcpg95@gmail.com>
2024-05-20 18:21:59 +0900

    FEAT : 토큰 삭제 커스텀 훅의 로그인 화면으로 이동 로직 추가

--- src/hooks/auth/auth.hook.ts
+++ src/hooks/auth/auth.hook.ts
@@ -1,12 +1,16 @@
+import {NavigationProps} from './../../types/stackprops';
 import {useRecoilState} from 'recoil';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useState, useEffect} from 'react';
 import loginState from '@/recoil/auth/loginInfo.recoil';
+import {useHomeScreenHooks} from '../home/home.hook';
+import {useNavigation} from '@react-navigation/native';
 
 export const useAuthHook = () => {
+  const navigation = useNavigation();
   const [accessToken, setAccessToken] = useState<string | null>();
   const [state, setState] = useRecoilState(loginState);
-
+  const {handleHomeScreenNavReset} = useHomeScreenHooks();
   useEffect(() => {
     const fetchAccessToken = async () => {
       try {
@@ -20,7 +24,11 @@ export const useAuthHook = () => {
   }, []);
 
   const removeToken = async () => {
-    AsyncStorage.clear();
+    navigation.reset({
+      index: 0,
+      routes: [{name: 'Login' as never}], // 원하는 초기 화면으로 설정
+    });
+    return await AsyncStorage.clear();
   };
 
   const handleLoginModal = (value: boolean) => {
