e429979454567950a8c51b96a164494e3cc1400e
JoEunsoo <dcpg95@gmail.com>
2024-05-22 03:08:32 +0900

    STYLE : 지도 컴포넌트 styled 구현

---
+++ src/styles/map/map.style.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+const Layout = styled.View`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+
+const Overlay = styled.Pressable`
+`;
+
+const MapMarker = styled.Pressable`
+`;
+
+export {Layout, Overlay, MapMarker};
