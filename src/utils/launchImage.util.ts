0a2dcd08d5163cf9472f01de6a65129423765c41
JoEunsoo <dcpg95@gmail.com>
2024-05-22 03:10:56 +0900

    CHORE : 앱 이미지 선택 로직 주석 추가

--- src/utils/launchImage.util.ts
+++ src/utils/launchImage.util.ts
@@ -5,6 +5,16 @@ import {
 } from 'react-native-image-picker';
 import {Alert} from 'react-native';
 
+/**
+ * 이미지 선택 기능
+ * - option : 받아오려는 이미지 타입, 사이즈 등 설정
+ * - launchImageLibrary : 메인 로직으로 사진첩에 접근한 뒤 이미지 파일을 받아온다.
+ *    - didCancel - 이미지 선택 기능 실행 후 취소 버튼 클릭시 true 값을 반환
+ *
+ * @param {any} updatedProfilePhoto - 프로필 사진 업뎃 api 요청 hook
+ * @returns 받아온 이미지 객체를 updatedProfilePhoto hook 으로 보내어 api 요청
+ * @err 선택한 이미지 객체의 key인 fileSize가 존재하지 않으면 alert
+ */
 export const leanchSelectImage = async (
   updatedProfilePhoto: (imageData: any) => Promise<void>,
 ) => {
@@ -16,6 +26,10 @@ export const leanchSelectImage = async (
     selectionLimit: 5,
   };
   await launchImageLibrary(options, res => {
+    if (res.didCancel) {
+      return;
+    }
+
     if (!res?.assets?.[0]?.fileSize) {
       return Alert.alert('존재하지 않는 이미지 파일입니다.');
     }
@@ -25,6 +39,6 @@ export const leanchSelectImage = async (
       type: res?.assets?.[0]?.type,
       uri: res?.assets?.[0]?.uri,
     };
-    updatedProfilePhoto(imageData);
+    return updatedProfilePhoto(imageData);
   });
 };
