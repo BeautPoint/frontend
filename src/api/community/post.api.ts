f612f066d4686a54c15a5c15b1017cc423ecfa9c
JoEunsoo <dcpg95@gmail.com>
2024-05-28 16:08:10 +0900

    FEAT : 게시물 삭제 api 요청 로직과 게시물 정렬 로직 추가

--- src/api/community/post.api.ts
+++ src/api/community/post.api.ts
@@ -2,27 +2,31 @@ import {useEffect, useState} from 'react';
 import {PostAPI} from '@/config/api.config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useQuery} from '@tanstack/react-query';
+import {Alert} from 'react-native';
 
 export const useCommunityQuery = () => {
   const [postData, setPostData] = useState<any>();
-  useEffect(() => {
-    const getPost = async () => {
-      try {
-        const {data} = await PostAPI.get('', {
-          headers: {
-            Accept: 'application/json',
-            'content-Type': 'application/json',
-          },
-        });
-        setPostData(data);
-      } catch (err) {
-        return err;
-      }
-    };
-    getPost();
-  }, []);
 
-  return {postData};
+  const getPost = async () => {
+    try {
+      const {data} = await PostAPI.get('', {
+        headers: {
+          Accept: 'application/json',
+          'content-Type': 'application/json',
+        },
+      });
+      const sortByDate = data?.sort(
+        (a, b) =>
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+      );
+
+      setPostData(sortByDate);
+    } catch (err) {
+      return err;
+    }
+  };
+
+  return {postData, getPost};
 };
 
 export const createPost = async (postData: any) => {
@@ -68,5 +72,22 @@ export const getPostById = async () => {
     return response.data;
   } catch (err: any) {
     console.log(err.response.data);
+  }
+};
+
+export const deletePost = async (postId: string) => {
+  const acccess_token = await AsyncStorage.getItem('access_token');
+  try {
+    const response = await PostAPI.delete(`/${postId}`, {
+      headers: {
+        Authorization: `Bearer ${acccess_token}`,
+      },
+    });
+
+    return response.data;
+  } catch (err: any) {
+    console.log(err.response.data);
+
+    return Alert.alert('요청을 실패하였습니다. 다시 시도해주세요.');
   }
 };
