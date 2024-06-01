54fbf16dfe13e75d30a30cb4d6d9b5bb03b8daf8
JoEunsoo <dcpg95@gmail.com>
2024-06-01 17:09:23 +0900

    REFACTOR : 탭 토큰 여부에 따른 화면 전환을 위한 로직 개선

--- src/navigation/tab.navigation.tsx
+++ src/navigation/tab.navigation.tsx
@@ -1,6 +1,7 @@
+import React, {useEffect} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Dimensions, Platform, Pressable, View} from 'react-native';
-import {useRecoilState, useRecoilValue} from 'recoil';
+import {Dimensions, Platform, View} from 'react-native';
+import {useRecoilState} from 'recoil';
 import navigationState from '@/recoil/navigation/navigation.recoil';
 import {AppText} from '@/styles/global.style';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -12,92 +13,97 @@ const Tab = createBottomTabNavigator();
 const device = Dimensions.get('window');
 
 function TabNavigation() {
-  console.log(device.height);
   const [navState, setNavState] = useRecoilState(navigationState);
-  const {accessToken} = useAuthHook();
+  const {accessToken, setAccessToken} = useAuthHook();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('access_token');
+      if (token) {
+        setAccessToken(token);
+      }
+    };
+
+    checkToken();
+  }, [setAccessToken]);
 
   return (
-    <>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: '#4D84E3', // 활성화된 탭의 라벨 색상
-          tabBarInactiveTintColor: '#8C939C',
-          tabBarStyle: {
-            backgroundColor: '#ffffff',
-            height: device.height > 800 ? '10%' : '11%',
-            paddingLeft: 10,
-            paddingRight: 10,
-          },
-          headerShown: false,
-        }}>
-        {navState.tabNavigationState.map(li => {
-          const iconName = ['홈', '커뮤니티'];
-          return (
-            <Tab.Screen
-              key={li.id}
-              name={li.name}
-              component={li.component}
-              listeners={({navigation}) => ({
-                tabPress: e => {
-                  if (li.name === '마이' && !accessToken) {
-                    e.preventDefault();
-                    navigation.navigate('Login');
-                  }
-                },
-              })}
-              options={{
-                tabBarIcon: ({focused}) => {
-                  return (
-                    <View
-                      style={{
-                        marginTop: 20,
-                        height: 30,
-                      }}>
-                      <li.icon
-                        color={
-                          focused && iconName.includes(li.name)
-                            ? '#ffffff'
-                            : focused
-                            ? '#4D84E3'
-                            : '#777777'
-                        }
-                        width={20}
-                        height={20}
-                        fill={focused ? '#4D84E3' : 'none'}
-                        strokeWidth={1}
-                      />
-                    </View>
-                  );
-                },
-                tabBarLabel: ({focused, color}) => (
-                  <View
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#4D84E3',
+        tabBarInactiveTintColor: '#8C939C',
+        tabBarStyle: {
+          backgroundColor: '#ffffff',
+          height: device.height > 800 ? '10%' : '11%',
+          paddingLeft: 10,
+          paddingRight: 10,
+        },
+        headerShown: false,
+      }}>
+      {navState.tabNavigationState.map(li => {
+        const iconName = ['홈', '커뮤니티'];
+        return (
+          <Tab.Screen
+            key={li.id}
+            name={li.name}
+            component={li.component}
+            listeners={({navigation}) => ({
+              tabPress: e => {
+                if (li.name === '마이' && !accessToken) {
+                  e.preventDefault();
+                  navigation.navigate('Login');
+                }
+              },
+            })}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <View
+                  style={{
+                    marginTop: 20,
+                    height: 30,
+                  }}>
+                  <li.icon
+                    color={
+                      focused && iconName.includes(li.name)
+                        ? '#ffffff'
+                        : focused
+                        ? '#4D84E3'
+                        : '#777777'
+                    }
+                    width={20}
+                    height={20}
+                    fill={focused ? '#4D84E3' : 'none'}
+                    strokeWidth={1}
+                  />
+                </View>
+              ),
+              tabBarLabel: ({focused, color}) => (
+                <View
+                  style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginBottom: Platform.OS === 'android' ? 15 : 5,
+                  }}>
+                  <AppText
                     style={{
-                      flex: 1,
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      marginBottom: Platform.OS === 'android' ? 15 : 5,
+                      height: 20,
+                      marginTop: Platform.OS === 'android' && focused ? -1 : 0,
+                      color,
+                      fontFamily: focused
+                        ? 'Pretendard-ExtraBold'
+                        : 'Pretendard',
+                      fontSize: 12,
                     }}>
-                    <AppText
-                      style={{
-                        height: 20,
-                        marginTop:
-                          Platform.OS === 'android' && focused ? -1 : 0,
-                        color,
-                        fontFamily: focused
-                          ? 'Pretendard-ExtraBold'
-                          : 'Pretendard',
-                        fontSize: 12,
-                      }}>
-                      {li.name}
-                    </AppText>
-                  </View>
-                ),
-              }}
-            />
-          );
-        })}
-      </Tab.Navigator>
-    </>
+                    {li.name}
+                  </AppText>
+                </View>
+              ),
+            }}
+          />
+        );
+      })}
+    </Tab.Navigator>
   );
 }
 
