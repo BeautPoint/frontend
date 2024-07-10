be048af33e13a1eea0a81fe3fdfb1afae92a70f5
JoEunsoo <dcpg95@gmail.com>
2024-07-11 00:00:49 +0900

    REFACTOR : home 화면의 배너 컴포넌트 무한 캐러셀 구현 및 dots 수정

--- src/components/home/bannner.component.tsx
+++ src/components/home/bannner.component.tsx
@@ -1,61 +1,117 @@
 import {SlideDots, SlideDotsBox} from '@/styles/common/slideDots.style';
 import * as S from '@/styles/home/banner.style';
-import {useRef, useState} from 'react';
-import {Dimensions, FlatList, View} from 'react-native';
+import {useCallback, useEffect, useRef, useState} from 'react';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  View,
+} from 'react-native';
+import banner1 from '@/assets/images/banner1-1.png';
+import banner2 from '@/assets/images/banner1-2.png';
+import {AppText} from '@/styles/global.style';
+
+const images = [
+  {
+    id: 0,
+    img: banner1,
+    mainText: '오래걸렸던 시술 샵 찾기 이젠 쉽고 간편하게!',
+    subText: '지금 바로 갈 수 있는 내 근처 샵 확인하기',
+  },
+
+  {
+    id: 1,
+    img: banner2,
+    mainText: '말하기 어려웠던 고민이나 정보 공유도 바로 여기서!',
+    subText: '지금 바로 커뮤니티 확인하기',
+  },
+];
 
 function HomeBanner() {
-  const [currentItemId, setCurrentItemId] = useState();
+  const [currentItemId, setCurrentItemId] = useState(0);
+  const flatListRef = useRef<FlatList>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const {width} = Dimensions.get('screen');
-  const images = [
-    {
-      id: 0,
-      url: 'https://www.shutterstock.com/image-vector/example-red-square-grunge-stamp-260nw-327662909.jpg',
-    },
-    {
-      id: 1,
-      url: 'https://img.freepik.com/premium-photo/wooden-cubes-with-word-example_284815-518.jpg',
-    },
-    {
-      id: 2,
-      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNRmkkAAs3nLaT10JYGUrVoeytP0u42BiK3AiiHhCy&s',
-    },
-  ];
 
-  const viewChangedHandle = useRef(({viewableItems}: any) => {
+  const handleViewableItems = useRef(({viewableItems}: any) => {
     if (viewableItems.length > 0) {
       setCurrentItemId(viewableItems[0].index);
     }
   }).current;
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex(prevIndex => {
+        const nextIndex = prevIndex + 1;
+        if (nextIndex === images.length + 1) {
+          flatListRef.current?.scrollToOffset({offset: 0, animated: false});
+          setCurrentIndex(1);
+          flatListRef.current?.scrollToIndex({index: 1, animated: true});
+          return 1;
+        }
+        flatListRef.current?.scrollToIndex({index: nextIndex, animated: true});
+        return nextIndex;
+      });
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const onScrollEnd = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const contentOffsetX = e.nativeEvent.contentOffset.x;
+    const index = Math.round(contentOffsetX / (width * 0.9));
+    const actualIndex = index % images.length;
+    setCurrentIndex(actualIndex);
+    if (index === images.length) {
+      flatListRef.current?.scrollToIndex({index: 0, animated: false});
+    }
+  };
 
   return (
     <S.BannerLayout width={width * 0.9}>
       <FlatList
         horizontal
         pagingEnabled
-        onViewableItemsChanged={viewChangedHandle}
-        data={images}
+        ref={flatListRef}
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={handleViewableItems}
+        data={[...images, {...images[0], id: 2}]} // Encountered two children with the same key 이슈로 id 제설정
+        keyExtractor={item => item.id.toString()}
+        onMomentumScrollEnd={onScrollEnd}
         renderItem={({item}) => (
           <View>
-            <S.Image
-              width={width * 0.9}
+            <Image
+              style={{width: width * 0.9}}
               key={item.id}
-              source={{uri: item.url}}
+              source={item.img}
             />
-            <S.GradientImage
-              start={{x: 0, y: 0}}
-              end={{x: 0, y: 1}}
-              colors={['rgba(0, 0, 0, 0.00)', 'rgba(0, 0, 0, 0.5)']}
-            />
+            <S.TextBox>
+              <S.MainText>
+                <AppText size="18px" color="#ffffff" weight="SemiBold">
+                  {item.mainText}
+                </AppText>
+              </S.MainText>
+              <S.SubText>
+                <AppText size="11px" color="#ffffff">
+                  {item.subText}
+                </AppText>
+              </S.SubText>
+            </S.TextBox>
           </View>
         )}
       />
-      <SlideDotsBox>
+      <SlideDotsBox bottom={7}>
         {images.length > 1 &&
-          images.map(item => (
+          images.map((item, index) => (
             <SlideDots
               size="6px"
-              background={item.id === currentItemId ? '#ffffff' : '#b5b5b5'}
+              background={
+                index === currentIndex % images.length ? '#ffffff' : '#b5b5b5'
+              }
               key={item.id}
+              margin={5}
             />
           ))}
       </SlideDotsBox>
