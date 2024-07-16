65774dc4565e2f80257b967f2d281118491047fd
JoEunsoo <dcpg95@gmail.com>
2024-07-16 23:14:51 +0900

    REFACTOR : Recoil 상태 관리와 useEffect를 활용한 커뮤니티 API 호출 구조 개선

--- src/api/community/post.api.ts
+++ src/api/community/post.api.ts
@@ -1,32 +1,46 @@
+import {useRecoilState} from 'recoil';
 import {useEffect, useState} from 'react';
 import {PostAPI} from '@/config/api.config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useQuery} from '@tanstack/react-query';
 import {Alert} from 'react-native';
+import communityState from '@/recoil/community/community.recoil';
 
 export const useCommunityQuery = () => {
-  const [postData, setPostData] = useState<any>();
+  const [postData, setPostData] = useState<any>([]);
+  const [state, setState] = useRecoilState(communityState);
 
-  const getPost = async () => {
-    try {
-      const {data} = await PostAPI.get('', {
-        headers: {
-          Accept: 'application/json',
-          'content-Type': 'application/json',
-        },
-      });
-      const sortByDate = data?.sort(
-        (a, b) =>
-          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
-      );
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        const {data} = await PostAPI.get('/');
 
-      setPostData(sortByDate);
-    } catch (err) {
-      return err;
-    }
-  };
+        const sortByDate = data?.sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+        );
+        setPostData(sortByDate);
+      } catch (err: any) {
+        console.log('요청 오류: ', err);
+      }
+    };
+    getPost();
+  }, [state.refreshing]);
 
-  return {postData, getPost};
+  return {postData};
+};
+
+export const getPostsByKeyword = async (keyword: string) => {
+  try {
+    const response = await PostAPI.get(`/search?query=${keyword}`);
+    const sortByDate = response.data?.sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+    );
+    return sortByDate;
+  } catch (err: any) {
+    console.log(err.response.data);
+  }
 };
 
 export const createPost = async (postData: any) => {
@@ -37,7 +51,6 @@ export const createPost = async (postData: any) => {
         Authorization: `Bearer ${acccess_token}`,
       },
     });
-    console.log('createPost : ', response.data);
     return response.data;
   } catch (err: any) {
     console.log(err.response.data);
@@ -53,21 +66,27 @@ export const updatePost = async postData => {
         Authorization: `Bearer ${acccess_token}`,
       },
     });
-    console.log('createPost : ', response.data);
     return response.data;
   } catch (err: any) {
     console.log(err.response.data);
   }
 };
 
-export const getPostById = async () => {
+export const getUserPost = async () => {
   const acccess_token = await AsyncStorage.getItem('access_token');
+
+  const response = await PostAPI.get('/my', {
+    headers: {
+      Authorization: `Bearer ${acccess_token}`,
+    },
+  });
+  // useQuery({queryKey: ['userPosts']});
+  return response;
+};
+
+export const getPostById = async (post_id: string) => {
   try {
-    const response = await PostAPI.get('/my', {
-      headers: {
-        Authorization: `Bearer ${acccess_token}`,
-      },
-    });
+    const response = await PostAPI.get(`/${post_id}`, {});
     // useQuery({queryKey: ['userPosts']});
     return response.data;
   } catch (err: any) {
