093a1de4083e89e8809cd7bd68ff949957ac5054
JoEunsoo <dcpg95@gmail.com>
2024-07-21 23:19:41 +0900

    REFACTOR : 네비게이션 상태 처리 로직 useEffect로 분리 및 Naver Oauth 개발 모드에서만 표시되도록 수정

--- src/components/auth/signin.component.tsx
+++ src/components/auth/signin.component.tsx
@@ -9,25 +9,45 @@ import {useNaverOauth} from '@/hooks/auth/oauth.hooks';
 import {NavigationProps} from '@/types/stackprops';
 import {useRecoilValue} from 'recoil';
 import navigationState from '@/recoil/navigation/navigation.recoil';
+import {useEffect} from 'react';
 
 function SignIn({navigation}: NavigationProps['signup']) {
   const {logout} = useNaverOauth();
   const {singupScreen, resetToHomeScreen} = useRecoilValue(navigationState);
+  const developMode = __DEV__;
   console.log(singupScreen);
-  singupScreen ? navigation.navigate('Signup') : null;
-  resetToHomeScreen ? navigation.reset({routes: [{name: 'Home'}]}) : null;
+  useEffect(() => {
+    if (singupScreen) {
+      navigation.navigate('Signup');
+    }
+  }, [singupScreen, navigation]);
+
+  useEffect(() => {
+    if (resetToHomeScreen) {
+      navigation.reset({routes: [{name: 'Home'}]});
+    }
+  }, [resetToHomeScreen, navigation]);
   return (
     <S.SignInLayOut>
-      <KakaoOauthButton navigation={navigation} />
-      <NaverOauthButton navigation={navigation} />
+      <View style={{marginBottom: 10}}>
+        <KakaoOauthButton navigation={navigation} />
+      </View>
+      {/* 네이버 검수 문제로 잠시 보류. */}
+      {developMode && (
+        <View style={{marginBottom: 10}}>
+          <NaverOauthButton navigation={navigation} />
+        </View>
+      )}
       {/* <Pressable onPress={() => logout()}>
         <AppText>로그아웃</AppText>
       </Pressable> */}
-      {Platform.OS === 'android' ? (
-        <GoogleOauthButton navigation={navigation} />
-      ) : (
-        <AppleOauthButton />
-      )}
+      <View style={{marginBottom: 10}}>
+        {Platform.OS === 'android' ? (
+          <GoogleOauthButton navigation={navigation} />
+        ) : (
+          <AppleOauthButton />
+        )}
+      </View>
     </S.SignInLayOut>
   );
 }
