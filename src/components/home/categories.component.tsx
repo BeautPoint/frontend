44fdeaf5909edbfe1c7964c45979d97b2a00d889
JoEunsoo <dcpg95@gmail.com>
2024-08-10 23:29:26 +0900

    FEAT : 홈 화면 카테고리 클릭 시 검색 결과 화면으로 이동 기능 추가

--- src/components/home/categories.component.tsx
+++ src/components/home/categories.component.tsx
@@ -1,19 +1,30 @@
+import {useSearchHook} from '@/hooks/search/useSearch.hook';
 import HomeState from '@/recoil/home/home.recoil';
 import {AppText} from '@/styles/global.style';
 import * as S from '@/styles/home/categories.style';
+import {NavigationProps} from '@/types/stackprops';
 import {useRecoilValue} from 'recoil';
 
-function HomeCategories() {
+function HomeCategories({navigation}: NavigationProps['home']) {
   const {categoryList} = useRecoilValue(HomeState);
+  const {updateSearchKeyword, setShowResultsScreen} = useSearchHook();
+
+  const handlePressed = (keyword: string) => {
+    updateSearchKeyword(keyword);
+    navigation.navigate('CategoryResults');
+  };
+
   return (
     <S.CategoriesLayout>
       {categoryList.map(item => (
-        <S.CategoryBox key={item.id}>
+        <S.Categorybutton
+          onPress={() => handlePressed(item.name)}
+          key={item.id}>
           <S.IconSection>
             <item.icon />
           </S.IconSection>
           <AppText size="12px">{item.name}</AppText>
-        </S.CategoryBox>
+        </S.Categorybutton>
       ))}
     </S.CategoriesLayout>
   );
