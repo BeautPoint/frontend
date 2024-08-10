b7a3e2454b70a9641c8bbe1bf839d33f97a44ba4
JoEunsoo <dcpg95@gmail.com>
2024-08-10 23:28:46 +0900

    FEAT : 게시물 상세 화면에 댓글 입력 폼 및 키보드 처리 기능 추가

--- src/components/community/detail/postdetail.component.tsx
+++ src/components/community/detail/postdetail.component.tsx
@@ -8,38 +8,66 @@ import {useRecoilValue} from 'recoil';
 import communityState from '@/recoil/community/community.recoil';
 import {ReportDropdownBackground} from '@/styles/screens/community.style';
 import {useCommunityPosts} from '@/hooks/community/communityPosts.hook';
+import {
+  Dimensions,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import CommentForm from './commentForm';
+import {useRef} from 'react';
+import {useEffect} from 'react';
 
 function CommunityPostDetail({navigation}: NavigationProps['community']) {
-  const {detailPostApi, showReportDropdown, isEditMode} =
+  const {detailPostData, showReportDropdown, isEditMode, postCommentsData} =
     useRecoilValue(communityState);
   const {dropdownBackgroundHandle} = useCommunityPosts();
   isEditMode && navigation.navigate('CreatePost');
+  const {height} = Dimensions.get('screen');
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  const heightCalcurate = (type: string) => {
+    type === 'post' ? height * 0.65 : height * 0.3;
+  };
+
+  useEffect(() => {
+    scrollViewRef.current?.scrollToEnd({animated: true});
+  }, [postCommentsData]);
+
   return (
     <S.DetailLayout>
       <DetailHeader navigation={navigation} />
       <S.PostMainSection>
         <CommunityUserInfo
-          postId={detailPostApi?.post_id}
-          nickname={detailPostApi?.nickName}
-          viewCount={detailPostApi?.viewCount}
-          profileImage={detailPostApi?.profile_image}
-          createdAt={detailPostApi?.createdAt}
+          postId={detailPostData?.post_id}
+          nickname={detailPostData?.nickName}
+          viewCount={detailPostData?.viewCount}
+          profileImage={detailPostData?.profile_image}
+          createdAt={detailPostData?.createdAt}
         />
         <S.MainBox>
           <S.PostTitle>
             <AppText weight="SemiBold" size="18px">
-              {detailPostApi?.title}
+              {detailPostData?.title}
             </AppText>
           </S.PostTitle>
           <S.PostBody>
-            <AppText>{detailPostApi?.content}</AppText>
+            <AppText>{detailPostData?.content}</AppText>
           </S.PostBody>
         </S.MainBox>
+        {showReportDropdown ? (
+          <ReportDropdownBackground
+            onPress={() => dropdownBackgroundHandle()}
+          />
+        ) : null}
+        <S.CommentSection>
+          <PostComments />
+        </S.CommentSection>
       </S.PostMainSection>
-      <PostComments />
-      {showReportDropdown ? (
-        <ReportDropdownBackground onPress={() => dropdownBackgroundHandle()} />
-      ) : null}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <CommentForm navigation={navigation} />
+      </KeyboardAvoidingView>
     </S.DetailLayout>
   );
 }
