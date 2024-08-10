ef09f051c6b8f0db79a095699e4a67f2f6dd26ed
JoEunsoo <dcpg95@gmail.com>
2024-08-10 23:29:42 +0900

    REFACTOR : 홈 화면 헤더에서 로고 및 알림 버튼 제거, 검색 입력 컴포넌트 수정

--- src/components/home/homeHeader.component.tsx
+++ src/components/home/homeHeader.component.tsx
@@ -1,28 +1,15 @@
-import {AppText} from '@/styles/global.style';
 import * as S from '@/styles/home/homeHeader.style';
-import Notification from '@/assets/icons/bellIcon.svg';
-import SearchInput from '@/components/common/searchinput.component';
+
 import {NavigationProps} from '@/types/stackprops';
 
-function HomeHeader({navigation}: NavigationProps['home']) {
-  const searchHandle = () => {
-    navigation.navigate('HomeSearch');
-  };
+import HomeSearchInput from './searchInput.component';
 
+
+function HomeHeader({navigation}: NavigationProps['home']) {
   return (
     <S.HeaderLayout>
-      <S.TopBox>
-        <S.LogoIcon>
-          <AppText size="22px">Logo</AppText>
-        </S.LogoIcon>
-        <S.NotificationButton>
-          <Notification color="black" width="17px" />
-        </S.NotificationButton>
-      </S.TopBox>
       <S.BottomBox>
-        <S.SearchBox onPress={() => searchHandle()}>
-          <SearchInput navigation={navigation} />
-        </S.SearchBox>
+        <HomeSearchInput navigation={navigation} />
       </S.BottomBox>
     </S.HeaderLayout>
   );
