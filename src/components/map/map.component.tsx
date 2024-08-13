4fcb44df86e3d1d97bd71b7d5ef6a5c2166fc615
JoEunsoo <dcpg95@gmail.com>
2024-08-13 23:31:18 +0900

    FEAT : 지도 화면에 현재 위치 이동 및 선택된 마커 하이라이트 기능 추가

--- src/components/map/map.component.tsx
+++ src/components/map/map.component.tsx
@@ -1,23 +1,58 @@
-import {useBottomSheetHook} from '@/hooks/shop/bottomsheet.hook';
-import {useShopService} from '@/hooks/shop/shopService.hook';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import * as S from '@/styles/map/map.style';
+import MarkerIcon from '@/assets/icons/marker.svg';
+import MapMarkerPNG from '@/assets/icons/marker.png';
+import {Image, PermissionsAndroid, Platform, Pressable} from 'react-native';
+import {useEffect, useRef, useState} from 'react';
+import Geolocation from '@react-native-community/geolocation';
+import MyLocationIcon from '@/assets/icons/gpsIcon.svg';
+import {useServiceHook} from '@/hooks/service/service.hook';
 
-function MapComponent({placesInfo}) {
+interface MapComponentProps {
+  isSheetOpen: boolean;
+  serviceInfo: [
+    {
+      place_id: string;
+      name: string;
+      phone: string;
+      address: string;
+      location: {longitude: number; latitude: number};
+      opening_hours: string[];
+      open_now: boolean;
+      photo?: string;
+      rating: number;
+      website?: string;
+      description?: string;
+    },
+  ];
+  setShowSheet: (value: boolean) => void;
+}
+
+function MapComponent({
+  isSheetOpen,
+  serviceInfo,
+  setShowSheet,
+}: MapComponentProps) {
   const GangNamgu = {
     latitude: 37.5212304114637,
     longitude: 127.022686095616,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  console.log('지도 : ', placesInfo);
-
-  const {handleShopDetails} = useShopService();
-  const {openBottomSheet} = useBottomSheetHook();
+  const [location, setLocation] = useState<{
+    latitude: number;
+    longitude: number;
+    latitudeDelta: number;
+    longitudeDelta: number;
+  } | null>(null);
+  const {handleShopDetails} = useServiceHook();
+  const mapRef = useRef<MapView | null>(null);
+  const [selectedMarker, setSelectedMarker] = useState<string | null>(null);
 
   const handlePressMarker = data => {
     handleShopDetails(data);
-    openBottomSheet(true);
+    setSelectedMarker(data.place_id);
+    setShowSheet(true);
   };
 
   const handleMapPress = event => {
@@ -25,36 +60,123 @@ function MapComponent({placesInfo}) {
     if (action === 'marker-press') {
       return;
     }
-    return openBottomSheet(false);
+    setSelectedMarker(null);
+    return setShowSheet(false);
   };
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      if (Platform.OS === 'android') {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Location Permission',
+            message: 'This app needs access to your location.',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('Location permission denied');
+          return;
+        }
+      }
+
+      Geolocation.getCurrentPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          setLocation({
+            latitude,
+            longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          });
+        },
+        error => {
+          console.error(error);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    };
+
+    requestLocationPermission();
+  }, []);
+
+  const handleMoveToLocation = () => {
+    console.log('현재위치 버튼 클릭, 위치: ', location); // 디버깅 로그 추가
+    if (location && mapRef.current) {
+      mapRef.current.animateToRegion(
+        {
+          ...location,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        },
+        1000,
+      ); // 1초 동안 애니메이션
+    } else {
+      console.log('location 또는 mapRef가 유효하지 않습니다.');
+    }
+  };
+
   return (
     <S.Layout>
       <MapView
+        ref={mapRef}
         style={{flex: 1, width: '100%', height: '100%'}}
         provider={PROVIDER_GOOGLE}
-        customMapStyle={[
-          {
-            featureType: 'poi.business',
-            stylers: [{visibility: 'off'}],
-          },
-        ]}
+        // customMapStyle={[
+        //   {
+        //     featureType: 'all',
+        //     elementType: 'labels',
+        //     stylers: [{visibility: 'off'}],
+        //   },
+        // ]}
         initialRegion={{
           ...GangNamgu,
         }}
         onPress={handleMapPress}>
-        {placesInfo?.map((item, i) => {
-          console.log(item);
+        {serviceInfo?.map((item, i) => {
+          if (!item?.location) return null;
           return (
             <Marker
               style={{zIndex: 5}}
-              key={i}
+              key={item?.place_id}
               coordinate={item?.location}
-              onPress={() => handlePressMarker(item)}
-            />
+              onPress={() => handlePressMarker(item)}>
+              <Image
+                source={
+                  selectedMarker === item.place_id
+                    ? require('@/assets/images/marker2.png')
+                    : require('@/assets/images/marker.png')
+                }
+                style={{
+                  height: 30,
+                  width: 30,
+                }}
+              />
+              {/* <MarkerIcon width={'25px'} /> */}
+            </Marker>
           );
         })}
-        {/* <Marker coordinate={GangNamgu} onPress={() => cons} /> */}
+        {/* <Marker coordinate={GangNamgu} /> */}
       </MapView>
+      <Pressable
+        style={{
+          position: 'absolute',
+          backgroundColor: '#ffffff',
+          borderRadius: 50,
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 30,
+          height: 30,
+          top: Platform.OS === 'android' ? 65 : 115,
+          right: 20,
+          zIndex: 1,
+        }}
+        onPress={handleMoveToLocation}>
+        <MyLocationIcon />
+      </Pressable>
     </S.Layout>
   );
 }
