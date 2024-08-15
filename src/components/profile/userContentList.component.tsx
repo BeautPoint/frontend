8441ab002858446e4c45c3d7d2fb4d06658d9041
JoEunsoo <dcpg95@gmail.com>
2024-08-15 23:34:32 +0900

    FEAT : 나의 작성 글과 댓글 탭에 따른 게시물 및 댓글 리스트 연동, 컨텐츠 클릭 시 상세 화면 이동 기능 추가

--- src/components/profile/userContentList.component.tsx
+++ src/components/profile/userContentList.component.tsx
@@ -1,5 +1,7 @@
+import {usePostCommentQuery} from '@/api/community/comments.api';
 import {useCommunityPosts} from '@/hooks/community/communityPosts.hook';
 import communityState from '@/recoil/community/community.recoil';
+import userInfoState from '@/recoil/user/user.recoil';
 import {AppText} from '@/styles/global.style';
 import * as S from '@/styles/profile/userContent.style';
 import {NavigationProps} from '@/types/stackprops';
@@ -7,10 +9,11 @@ import {dateCalculator} from '@/utils/dateCalculator.util';
 import {useRecoilValue} from 'recoil';
 
 function UserContentList({navigation}: NavigationProps['profile']) {
-  const {userPosts} = useRecoilValue(communityState);
-  const {detailPostData, handleIsDetailScreen, setEditPostData} =
+  const {userPosts, userComments} = useRecoilValue(communityState);
+  const {activeTab} = useRecoilValue(userInfoState);
+  const {setPostDetails, fetchPostById, handleIsDetailScreen, setEditPostData} =
     useCommunityPosts();
-
+  const {getComments} = usePostCommentQuery();
   const handlePressedPost = (post: any) => {
     const postData = {
       post_id: post.post_id,
@@ -18,33 +21,74 @@ function UserContentList({navigation}: NavigationProps['profile']) {
       content: post.content,
     };
     handleIsDetailScreen(true);
-    detailPostData(post);
+    setPostDetails(post);
     setEditPostData(postData);
-    navigation.navigate('Community');
+    getComments(post.post_id);
+    navigation.navigate('PostDetails');
   };
+
+  const handleCommentPress = async (post_id: string) => {
+    const postData = await fetchPostById(post_id);
+    setPostDetails(postData);
+    getComments(post_id);
+    navigation.navigate('PostDetails');
+  };
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return dateCalculator(date);
+  };
+
+  console.log('유저 게시물 : ', userPosts);
+
   return (
     <S.UserContnetLayout>
-      {userPosts.map(item => {
-        const date = new Date(item.createdAt);
-        const formattedDate = dateCalculator(date);
-        return (
-          <S.ContnetBox onPress={() => handlePressedPost(item)}>
-            <S.Title>
-              <AppText>{item.title}</AppText>
-            </S.Title>
-            <S.Description>
-              <AppText numberOfLines={2} size="13px">
-                {item.content}
-              </AppText>
-            </S.Description>
-            <S.CreatedDate>
-              <AppText size="12px" color="#9ea2a7">
-                {formattedDate}
-              </AppText>
-            </S.CreatedDate>
-          </S.ContnetBox>
-        );
-      })}
+      {activeTab === 0 && userPosts[0] ? (
+        userPosts.map(item => {
+          const formattedDate = formatDate(item.createdAt);
+          return (
+            <S.ContnetBox
+              key={item.post_id}
+              onPress={() => handlePressedPost(item)}>
+              <S.Title>
+                <AppText>{item.title}</AppText>
+              </S.Title>
+              <S.Description>
+                <AppText numberOfLines={2} size="13px">
+                  {item.content}
+                </AppText>
+              </S.Description>
+              <S.CreatedDate>
+                <AppText size="12px" color="#9ea2a7">
+                  {formattedDate}
+                </AppText>
+              </S.CreatedDate>
+            </S.ContnetBox>
+          );
+        })
+      ) : activeTab === 1 && userComments[0] ? (
+        userComments.map(item => {
+          const formattedDate = formatDate(item.createdAt);
+          return (
+            <S.ContnetBox
+              key={item.comment_id}
+              onPress={() => handleCommentPress(item.post_id)}>
+              <S.Description>
+                <AppText numberOfLines={2} size="13px">
+                  {item.content}
+                </AppText>
+              </S.Description>
+              <S.CreatedDate>
+                <AppText size="12px" color="#9ea2a7">
+                  {formattedDate}
+                </AppText>
+              </S.CreatedDate>
+            </S.ContnetBox>
+          );
+        })
+      ) : (
+        <AppText>등록된 글이 없습니다.</AppText>
+      )}
     </S.UserContnetLayout>
   );
 }
