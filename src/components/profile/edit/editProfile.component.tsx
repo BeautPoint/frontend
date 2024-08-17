767f08660fd9540062b045db22c4bb1b4ecc6320
JoEunsoo <dcpg95@gmail.com>
2024-08-17 23:35:25 +0900

    FEAT : 프로필 수정 화면의 사용자 프로필 정보 표시와 수정할 수 있도록 API 연동 기능 추가

--- src/components/profile/edit/editProfile.component.tsx
+++ src/components/profile/edit/editProfile.component.tsx
@@ -10,6 +10,7 @@ import userInfoState from '@/recoil/user/user.recoil';
 import {useRecoilValue} from 'recoil';
 import EditIcon from '@/assets/icons/pencelIcon.svg';
 import {useActionSheetHook} from '@/hooks/common/actionSheet.hooks';
+import {useFetchUserProfile} from '@/api/user/userInfo.api';
 
 function EditProfile({navigation}: NavigationProps['profile']) {
   const {updateMenuList, openActionSheet} = useActionSheetHook();
@@ -17,19 +18,25 @@ function EditProfile({navigation}: NavigationProps['profile']) {
     updateMenuList('image');
     openActionSheet(true);
   };
+
   const defaultBirth = new Date(2001, 1, 1);
-  const mokdata = [
+
+  const birthDateOptions = [
     {id: 0, value: '2000'},
     {id: 1, value: '12'},
     {id: 2, value: '04'},
   ];
-  const mokdata2 = [
+
+  const genderOptions = [
     {id: 0, value: '여자'},
     {id: 1, value: '남자'},
   ];
 
   const select = '여자';
   const {userProfile} = useRecoilValue(userInfoState);
+  const {profileData, editProfile} = useFetchUserProfile();
+
+  console.log('프로필 수정 페이지 : ', userProfile);
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
@@ -39,12 +46,12 @@ function EditProfile({navigation}: NavigationProps['profile']) {
       </S.Header>
       <S.MainContent>
         <S.ImageSection>
-          <S.SelectImageButton onPress={() => handleSelectImage()}>
+          <S.SelectImageButton onPress={handleSelectImage}>
             <S.ProfilePhoto
               source={
-                !userProfile?.profile_image
+                !profileData?.profile_image
                   ? DefaultProfile
-                  : {uri: userProfile?.profile_image}
+                  : {uri: profileData?.profile_image}
               }
             />
             <S.Icon>
@@ -57,19 +64,19 @@ function EditProfile({navigation}: NavigationProps['profile']) {
           <S.Label>
             <AppText>닉네임</AppText>
           </S.Label>
-          <S.TextInput defaultValue={userProfile?.nickName} />
+          <S.TextInput defaultValue={profileData?.nickName} />
         </S.EditItem>
         <S.EditItem>
           <S.Label>
             <AppText>생년월일</AppText>
           </S.Label>
           <S.DateBox>
-            {mokdata.map(item => {
+            {birthDateOptions.map(item => {
               return (
                 <S.birthDateButton
                   key={item.id}
                   onPress={() => setIsOpen(true)}>
-                  <AppText color={'#5a6068'}>{item.value}</AppText>
+                  <AppText color="#5a6068">{item.value}</AppText>
                   <DownIcon />
                 </S.birthDateButton>
               );
@@ -81,8 +88,8 @@ function EditProfile({navigation}: NavigationProps['profile']) {
             <AppText>성별</AppText>
           </S.Label>
           <S.GenderBox>
-            {mokdata2.map(item => {
-              const color = select === item.value ? '#619BFF' : 'none';
+            {genderOptions.map(item => {
+              const color = select === item.value ? '#619BFF' : '';
               const weight = select === item.value ? 'SemiBold' : '';
               return (
                 <S.GenderButton key={item.id} border={color}>
