2a6e76698dbaf2acb9548f40e077f1728dfb45a1
JoEunsoo <dcpg95@gmail.com>
2024-08-21 01:02:23 +0900

    REFACTOR : 사업장 상세 정보 로직 확장 및 운영시간, 사진, 전화번호 등 추가 정보 처리 로직 적용

--- src/hooks/shop/shopService.hook.ts
+++ src/hooks/shop/shopService.hook.ts
@@ -1,4 +1,8 @@
 import shopState from '@/recoil/shop/shop.recoil';
+import {
+  formatOpeningHours,
+  getTodayOpeningHours,
+} from '@/utils/openingHours.util';
 import {regExCheckAddress, regExCheckName} from '@/utils/validateShop.util';
 import {useRecoilState} from 'recoil';
 
@@ -6,13 +10,44 @@ export const useShopService = () => {
   const [state, setState] = useRecoilState(shopState);
 
   const handleShopDetails = (shopData: any) => {
-    const {name, formatted_address, photo} = shopData;
-    const address = regExCheckAddress(formatted_address);
+    const {
+      place_id,
+      name,
+      address,
+      photo,
+      description,
+      open_now,
+      opening_hours,
+      phone,
+    } = shopData;
+    const formattedAddress = regExCheckAddress(address);
+    const openingHours = formatOpeningHours(opening_hours) || {
+      weekdays: '',
+      differentWeekdays: '',
+      weekends: '',
+      differentWeekends: '',
+      closed: '',
+    };
+    const todayOpeningHours = getTodayOpeningHours(opening_hours);
     const updatedName = regExCheckName(name);
+    const updateSelecedService = {
+      place_id,
+      name: updatedName,
+      formattedAddress,
+      images: [{id: 0, src: photo}],
+      description,
+      todayOpeningHours,
+      open_now,
+      openingHours,
+      phone,
+    };
 
     setState(prevState => ({
       ...prevState,
-      selectedShop: {name: updatedName, address, photo},
+      selectedService: {
+        ...prevState.selectedService,
+        ...updateSelecedService,
+      },
     }));
   };
 
