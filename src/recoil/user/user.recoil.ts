225446b4525abc19a48011bcd297cc5a04fd87a3
JoEunsoo <dcpg95@gmail.com>
2024-08-28 01:07:35 +0900

    ADD : 유저화면의 recoil 탭 상태관리 추가

--- src/recoil/user/user.recoil.ts
+++ src/recoil/user/user.recoil.ts
@@ -4,6 +4,11 @@ const userInfoState = atom({
   key: `userInfoState/${randomString()}`,
   default: {
     userProfile: {nickName: '', profile_image: '', socialType: ''},
+    profiletabInfo: [
+      {id: 0, text: '나의 글'},
+      {id: 1, text: '나의 댓글'},
+    ],
+    activeTab: 0,
   },
 });
 
