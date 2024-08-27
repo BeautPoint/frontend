875e1568e17da3be0157148b9760f29febf5a064
JoEunsoo <dcpg95@gmail.com>
2024-08-28 01:07:03 +0900

    REFACTOR : 네비게이션 recoil의 탭 상태 컴포넌트 타입 수정 및 탭 바 높이, 검색 관련 상태 값 추가

--- src/recoil/navigation/navigation.recoil.ts
+++ src/recoil/navigation/navigation.recoil.ts
@@ -1,9 +1,4 @@
 import {atom} from 'recoil';
-import CommunityScreen from '@/screens/main/community.screen';
-import HomeScreen from '@/screens/main/home.screen';
-import LocationScreen from '@/screens/main/location.screen';
-import ProfileScreen from '@/screens/main/profile.screen';
-import WishlistScreen from '@/screens/main/wishlist.screen';
 import HomeIcon from '@/assets/icons/homeIcon.svg';
 import LocationIcon from '@/assets/icons/locationIcon.svg';
 import CommunityIcon from '@/assets/icons/communityIcon.svg';
@@ -16,23 +11,26 @@ const navigationState = atom({
   default: {
     modalOpen: false,
     tabNavigationState: [
-      {id: 0, name: '홈', component: HomeScreen, icon: HomeIcon},
-      {id: 1, name: '내주변', component: LocationScreen, icon: LocationIcon},
+      {id: 0, name: '홈', component: 'HomeScreen', icon: HomeIcon},
+      {id: 1, name: '내주변', component: 'LocationScreen', icon: LocationIcon},
       {
         id: 2,
         name: '커뮤니티',
-        component: CommunityScreen,
+        component: 'CommunityScreen',
         icon: CommunityIcon,
       },
-      {id: 3, name: '찜', component: WishlistScreen, icon: LikeIcon},
-      {id: 4, name: '마이', component: ProfileScreen, icon: ProfileIcon},
+      {id: 3, name: '찜', component: 'WishlistScreen', icon: LikeIcon},
+      {id: 4, name: '마이', component: 'ProfileScreen', icon: ProfileIcon},
     ],
-    searchScreenType: 'Home',
+    navCurrentLocation: 'home',
+    tabBarHeight: 0,
     confirmBottomSheet: false,
     singupScreen: false,
     resetToHomeScreen: false,
-    currentScreen: '',
     headerTitle: '',
+    searchKeyword: '',
+    recentSearches: [] as string[],
+    showResultsScreen: false,
   },
 });
 
