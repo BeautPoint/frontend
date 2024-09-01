8a51f3115a17da1d8aa74b715670dae98cff9553
JoEunsoo <dcpg95@gmail.com>
2024-09-02 01:10:40 +0900

    REFACTOR : 불필요한 코드 제거,  바텀시트 조건부 렌더링 추가 및 지도 컴포넌트 props 값 수정

--- src/screens/main/location.screen.tsx
+++ src/screens/main/location.screen.tsx
@@ -1,13 +1,16 @@
-import BottomSheet from '@/components/common/bottomsheet.component';
 import MapComponent from '@/components/map/map.component';
 import * as S from '@/styles/screens/location.style';
 import {NavigationProps} from '@/types/stackprops';
 import SearchView from '@/screens/search/searchview.screen';
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import {Dimensions} from 'react-native';
 import MyLocationIcon from '@/assets/icons/gpsIcon.svg';
-import SearchInput from '@/components/common/searchinput.component';
+import HomeSearchInput from '@/components/home/searchInput.component';
 import {getPlace, usePlaces} from '@/api/google/gooogleCloud.api';
+import MapBottomSheet, {
+  useBottomSheetHook,
+} from '@/components/map/bottomSheet.component';
+import {useFetchService} from '@/api/shop/service.api';
 
 const {height, width} = Dimensions.get('screen');
 
@@ -15,9 +18,10 @@ function LocationScreen({navigation}: NavigationProps['location']) {
   const [isClick, setIsClick] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
-  const {placesInfo} = usePlaces();
-  console.log(placesInfo);
-
+  // const {placesInfo} = usePlaces();
+  // console.log(placesInfo);
+  const {serviceInfo} = useFetchService();
+  const {isSheetOpen, setShowSheet} = useBottomSheetHook();
   const handleHoverIn = () => {
     setIsHovered(true);
   };
@@ -36,18 +40,22 @@ function LocationScreen({navigation}: NavigationProps['location']) {
     <S.LocationLayOut>
       <S.SearchInputBox>
         <S.BoxShadow width={width * 0.9}>
-          <SearchInput navigation={navigation} screenType={'location'} />
+          <HomeSearchInput navigation={navigation} />
         </S.BoxShadow>
-        <S.MyLocation>
+        {/* <S.MyLocation>
           <MyLocationIcon />
-        </S.MyLocation>
+        </S.MyLocation> */}
       </S.SearchInputBox>
 
       {/* <SearchInput navigation={navigation} /> */}
       {isClick && <SearchView navigation={navigation} />}
-      <MapComponent placesInfo={placesInfo} />
-      <BottomSheet navigation={navigation} />
+      <MapComponent
+        serviceInfo={serviceInfo}
+        isSheetOpen={isSheetOpen}
+        setShowSheet={setShowSheet}
+      />
       {/* <DraggableBottomSheet /> */}
+      {isSheetOpen && <MapBottomSheet navigation={navigation} />}
     </S.LocationLayOut>
   );
 }
