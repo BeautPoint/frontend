970f93b341dc33ce67267b93e30ad14455f77408
JoEunsoo <dcpg95@gmail.com>
2024-09-02 01:11:32 +0900

    REFACTOR: 사용자 프로필, 게시물, 댓글 데이터를 React Query 캐싱 적용된 커스텀 훅으로 수정

--- src/screens/main/profile.screen.tsx
+++ src/screens/main/profile.screen.tsx
@@ -2,28 +2,29 @@ import * as S from '@/styles/screens/profile.style';
 import {NavigationProps} from '@/types/stackprops';
 import ContentOverView from '@/components/profile/contentOverview.component';
 import ProfileOverView from '@/components/profile/profileOverview.component';
-import {useUserInfoHook} from '@/hooks/user/userinfo.hook';
 import {useFocusEffect} from '@react-navigation/native';
 import {useCallback} from 'react';
 import SettingsDetails from '@/components/profile/setting/settingsDetail.component';
 import {useCommunityPosts} from '@/hooks/community/communityPosts.hook';
+import {usePostCommentQuery} from '@/api/community/comments.api';
+import {useFetchUserProfile} from '@/api/user/userInfo.api';
 
 function ProfileScreen({navigation}: NavigationProps['profile']) {
-  const {fetchUserProfile} = useUserInfoHook();
   const {fetchUserPosts} = useCommunityPosts();
+  const {getUserComments} = usePostCommentQuery();
+  const {profileData} = useFetchUserProfile();
+
   useFocusEffect(
     useCallback(() => {
-      fetchUserProfile();
       fetchUserPosts();
-      return;
+      getUserComments();
     }, []),
   );
-
   const showScreen = false;
 
   return (
     <S.ProfileLayOut>
-      <ProfileOverView navigation={navigation} />
+      <ProfileOverView navigation={navigation} profileData={profileData} />
       <ContentOverView navigation={navigation} />
       {showScreen && <SettingsDetails navigation={navigation} />}
     </S.ProfileLayOut>
